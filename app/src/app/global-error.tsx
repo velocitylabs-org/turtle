d25f73ec049389d18cfcccd471c22e7860b5a88a
Noah Joeris <noahjoeris@gmail.com>
2024-05-03 14:43:12 +0200

    setup: Integrate Sentry.io (#1)
    includes installing package, adding env, adding global-error, example sentry page and route
    
    Co-authored-by: Nuno <hi@nunoalexandre.com>

---
+++ app/src/app/global-error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
+interface GlobalErrorProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+const GlobalError: React.FC<GlobalErrorProps> = ({ error, reset }) => {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
