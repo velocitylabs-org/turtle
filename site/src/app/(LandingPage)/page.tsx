fd6d2867840a7b5b9fb2c581be0371b1fc6d79fa
Nuno <hi@nunoalexandre.com>
2024-05-16 10:39:50 +0200

    setup: separate site/ and app/ (#7)

--- site/src/app/(LandingPage)/page.tsx
+++ site/src/app/(LandingPage)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         </a>
 
         <Link
-          href={'/app'}
+          href={'#todo'}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className="mb-3 text-2xl font-semibold">
