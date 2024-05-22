cf48decb7198428e1fe58f3b1a79ae13a87646a6
Noah Joeris <noahjoeris@gmail.com>
2024-05-22 14:48:33 +0200

    cleanup truncate address

--- app/src/utils/address.ts
+++ app/src/utils/address.ts
@@ -1,7 +1,17 @@
-// TODO: cleanup and make more readable.
-export const truncateAddress = (str = '', start = 4, end = 4) =>
-  str && str.length
-    ? str.length <= start + end
-      ? str
-      : `${str.substring(0, start)}...` + (end > 0 ? str.substring(str.length - end) : '')
-    : null
+/**
+ * Truncate a blockchain address by showing the beginning and end parts.
+ *
+ * @param str - The address string to be truncated.
+ * @param start - Number of characters to show from the start. Default is 4.
+ * @param end - Number of characters to show from the end. Default is 4.
+ * @returns The truncated address or an empty string if the input string is empty.
+ */
+export const truncateAddress = (str: string, start: number = 4, end: number = 4) => {
+  if (!str || str.length === 0) return ''
+  if (str.length <= start + end) return str
+
+  const startStr = str.substring(0, start)
+  const endStr = end > 0 ? str.substring(str.length - end) : ''
+
+  return `${startStr}...${endStr}`
+}
