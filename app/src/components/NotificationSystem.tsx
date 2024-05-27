4888e280fc65ef25df8600329cd76458f721bf9d
Noah Joeris <noahjoeris@gmail.com>
2024-05-27 21:21:55 +0200

    notifications working

--- app/src/components/NotificationSystem.tsx
+++ app/src/components/NotificationSystem.tsx
@@ -1,14 +1,10 @@
-import { Notification } from '@/models/notification'
+'use client'
+import useNotification from '@/hooks/useNotification'
 import { AnimatePresence } from 'framer-motion'
-import { useState } from 'react'
 import NotificationToast from './NotificationToast'
 
 export default function NotificationSystem() {
-  const [notifications, setNotifications] = useState<Notification[]>([])
-
-  const removeNotification = (id: number) => {
-    setNotifications(pv => pv.filter(n => n.id !== id))
-  }
+  const { notifications, removeNotification } = useNotification()
 
   return (
     <div className="pointer-events-none fixed right-2 top-2 z-50 flex w-72 flex-col gap-1">
---
+++ app/src/hooks/useNotification.tsx
@@ -0,0 +1,11 @@
+import { useNotificationStore } from '@/store/notificationStore'
+
+const useNotification = () => {
+  const notifications = useNotificationStore(state => state.notifications)
+  const addNotification = useNotificationStore.getState().addNotification
+  const removeNotification = useNotificationStore.getState().removeNotification
+
+  return { notifications, addNotification, removeNotification }
+}
+
+export default useNotification
