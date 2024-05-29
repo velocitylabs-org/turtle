906e87f4bbc2fdde5c93d53aa4446bfece443264
Noah Joeris <noahjoeris@gmail.com>
2024-05-29 16:35:56 +0200

    add transfer services, hook and model

--- app/src/models/transfer.ts
+++ app/src/models/transfer.ts
@@ -1,14 +1,14 @@
 import { Chain } from './chain'
 import { Token } from './token'
 
-// TODO: Update the interface once it is more clearly defined
+// TODO: Update the interface once it is more clearly defined!
 export interface Transfer {
   id: string
   txHashes: string[]
   token: Token
   sourceChain: Chain
   destChain: Chain
-  amount: string
-  receiver: string
+  amount: bigint
+  receiver: string // TODO: Decide to use either string type or create own Address type
   status: string
 }
