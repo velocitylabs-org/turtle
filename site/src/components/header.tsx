1a9ea331c1f7a01f74d3e698e2a9e3bd6db8cb71
Victor-Salomon <victor-salomon@hotmail.fr>
2024-06-10 17:31:24 +0200

    wip - add header

---
+++ site/src/components/header.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link'
+import { TurtleSocialIcons } from './assets/social-icons'
+import { TurtleIcon } from './assets/turtle-icon'
+
+export default function Header() {
+  return (
+    <header className="fixed left-0 right-0 top-0 z-50 px-4 backdrop-blur-md lg:px-8 ">
+      <div className="flex items-center justify-between py-4">
+        <Link href="/" className="flex items-center space-x-2">
+          <TurtleIcon size={28} />
+          <p className="text-[32px] font-bold">Turtle</p>
+        </Link>
+
+        <nav>
+          {/* mobile menu */}
+          <div className="drawer md:hidden">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <label htmlFor="my-drawer" className="btn btn-circle btn-ghost drawer-button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+              </label>
+            </div>
+            {/* content mobile menu */}
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <div className="menu min-h-full w-2/3 space-y-4 bg-secondary p-4 text-base-content">
+                <div className="text-xl font-bold">🐢 Turtle</div>
+                <ul className="space-y-4">
+                  <li>
+                    <button className="btn btn-outline text-base font-normal">
+                      <a href="">Read the Docs</a>
+                    </button>
+                  </li>
+                  <li>
+                    <button className="btn btn-primary text-base font-normal">
+                      <a href="">Start a transfer</a>
+                    </button>
+                  </li>
+                </ul>
+                <div className="fixed inset-x-0 bottom-0 flex items-center justify-center space-x-4 pb-8">
+                  <TurtleSocialIcons />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* desktop menu */}
+          <ul className="hidden items-center space-x-6 md:flex">
+            <li>
+              <a href="">Read the Docs</a>
+            </li>
+            <li>
+              <button className="btn btn-primary text-base font-normal">
+                <a href="">Start a transfer</a>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
