5dcab4dd55b275b3261cd9d654eaa2d13d4a8d88
Noah Joeris <33572822+noahjoeris@users.noreply.github.com>
2024-06-11 14:03:20 +0200

    feat: adds meticulous ai for automatic e2e tests (#23)

--- app/sentry.client.config.ts
+++ app/sentry.client.config.ts
@@ -30,7 +30,7 @@ Sentry.init({
 
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
-      colorScheme: 'system',
+      colorScheme: 'light',
     }),
   ],
 })
