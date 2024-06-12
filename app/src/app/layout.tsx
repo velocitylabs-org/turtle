f145873bf4355be233373b5fd43b96cbd532b735
Noah Joeris <noahjoeris@gmail.com>
2024-06-12 12:11:10 +0200

    fix: nextjs hydration error

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -25,17 +25,17 @@ export default function RootLayout({
   const initialState = cookieToInitialState(config, headers().get('cookie'))
   return (
     <html lang="en">
+      <head>
+        {(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="SlFMjRvrnJwnEVBIlInfhVSOgIROWJD9Bp8Q2PTk"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </head>
       <body className={twMerge(inter.className, 'bg-turtle-tertiary')}>
-        <head>
-          {(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && (
-            // eslint-disable-next-line @next/next/no-sync-scripts
-            <script
-              data-project-id="SlFMjRvrnJwnEVBIlInfhVSOgIROWJD9Bp8Q2PTk"
-              data-is-production-environment="false"
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
-        </head>
         <Navbar />
         <NotificationSystem />
         <Web3ModalProvider initialState={initialState}>{children}</Web3ModalProvider>
