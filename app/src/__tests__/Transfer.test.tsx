03ac493f605df8665ebbcdb96497e8e908c665ae
Nuno Alexandre <hi@nunoalexandre.com>
2024-06-19 23:52:42 +0200

    Test toHumans

--- app/src/__tests__/Transfer.test.tsx
+++ app/src/__tests__/Transfer.test.tsx
@@ -1,7 +1,7 @@
 import { Direction, resolveDirection } from '@/services/transfer'
 import '@testing-library/jest-dom'
 import { Mainnet } from '../config/registry'
-import { convertAmount } from '@/utils/transfer'
+import { convertAmount, toHumans } from '@/utils/transfer'
 
 describe('Transfer', () => {
   it('direction ToEthereum', () => {
@@ -26,5 +26,13 @@ describe('Transfer', () => {
     expect(convertAmount(1, Mainnet.WETH)).toBe(BigInt(1000000000000000000))
     expect(convertAmount(0.12, Mainnet.WETH)).toBe(BigInt(120000000000000000))
     expect(convertAmount(123, Mainnet.WETH)).toBe(BigInt(123000000000000000000))
+  })
+
+  it('convert input amount to based back to humans  ', () => {
+    let inputs = [1, 10000, 123, 0.35]
+
+    inputs.forEach(x => {
+      expect(toHumans(convertAmount(x, Mainnet.WETH)!, Mainnet.WETH)).toBe(x)
+    })
   })
 })
