7bda724a9c927fade835db4f79ff0c55f6843466
Nuno Alexandre <hi@nunoalexandre.com>
2024-06-21 11:10:19 +0200

    Drop unused code

--- app/src/hooks/useOngoingTransfers.tsx
+++ app/src/hooks/useOngoingTransfers.tsx
@@ -1,8 +1,3 @@
-import { NotificationSeverity } from '@/models/notification'
-import { Transfer } from '@/models/transfer'
-import { getOngoingTransfers } from '@/services/ongoingTransfers'
-import * as Sentry from '@sentry/nextjs'
-import { useCallback, useEffect, useState } from 'react'
 import { useOngoingTransfersStore } from '@/store/ongoingTransfersStore'
 
 const useOngoingTransfers = () => {
@@ -12,48 +7,5 @@ const useOngoingTransfers = () => {
 
   return { ongoingTransfers, addTransfer, removeTransfer }
 }
-
-// const useOngoingTransfers = (address?: string) => {
-//   const { addNotification } = useNotification()
-//   // nuno: store here
-
-// const [ongoingTransfers, setOngoingTransfers] = useState<Transfer[]>([])
-// const [loading, setLoading] = useState<boolean>(true)
-// const [error, setError] = useState<string | null>(null)
-
-// const fetchOngoingTransfers = useCallback(async () => {
-//   try {
-//     setLoading(true)
-//     setError(null)
-
-//     const loadedTransfers: Transfer[] = await getOngoingTransfers(address)
-
-//     setOngoingTransfers(loadedTransfers)
-//   } catch (err) {
-//     let errorMessage = 'An unknown error occurred'
-//     if (err instanceof Error) errorMessage = err.message
-
-//     setError(errorMessage)
-//     addNotification({
-//       header: 'Error loading ongoing transfers',
-//       message: errorMessage,
-//       severity: NotificationSeverity.Error,
-//     })
-//     Sentry.captureException(err)
-//   } finally {
-//     setLoading(false)
-//   }
-// }, [address, addNotification])
-
-// useEffect(() => {
-//   fetchOngoingTransfers()
-// }, [fetchOngoingTransfers])
-
-// return {
-//   ongoingTransfers,
-//   loading,
-//   error,
-// }
-// }
 
 export default useOngoingTransfers
