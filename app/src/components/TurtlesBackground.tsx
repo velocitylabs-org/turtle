f61be4bb92eae76402a375dde9bab464d7967944
Victor Salomon <68683271+Victor-Salomon@users.noreply.github.com>
2024-06-24 17:02:58 +0200

    add wave background (#31)

--- app/src/components/TurtlesBackground.tsx
+++ app/src/components/TurtlesBackground.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { WaveLine } from './WavesBackground'
 
 interface TurtlesBackgroundProps {
   /** Background to display. */
@@ -12,6 +13,12 @@ export const TurtlesBackground: React.FC<TurtlesBackgroundProps> = ({ src, alt }
     <div className="absolute top-0 z-0">
       <div className="relative h-screen w-screen overflow-hidden">
         <Image src={src} alt={alt} fill sizes="100vw" className="object-cover" />
+        <WaveLine
+          name="blueBackground"
+          withStroke={false}
+          className="absolute inset-x-0 bottom-0 h-full w-full lg:h-auto"
+        />
+        <div className="absolute bottom-0 h-1/2 w-full bg-turtle-tertiary lg:hidden" />
       </div>
     </div>
   )
