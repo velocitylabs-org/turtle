bf9512575463c79a792ebc86e2307fc2e459e69f
Victor-Salomon <victor-salomon@hotmail.fr>
2024-06-27 10:39:38 +0200

    fix unused component to process to build

--- app/src/services/completedTransfers.ts
+++ app/src/services/completedTransfers.ts
@@ -1,4 +1,4 @@
-import { Transfer } from '@/models/transfer'
+import { StoredTransfer } from '@/models/transfer'
 
 /**
  * Fetches the finished transfers for a given address. Or for all addresses if no address is provided.
@@ -7,7 +7,7 @@ import { Transfer } from '@/models/transfer'
  * @returns A promise that resolves to completed transfers for the given address.
  * @throws An error if the fetch request fails.
  */
-export const getCompletedTransfers = async (address?: string): Promise<Transfer[]> => {
+export const getCompletedTransfers = async (address?: string): Promise<StoredTransfer[]> => {
   // TODO: adjust function parameters as needed once api works
   // add query params
   const searchParams = new URLSearchParams()
@@ -18,7 +18,7 @@ export const getCompletedTransfers = async (address?: string): Promise<Transfer[
   if (!response.ok)
     throw new Error(`${response.status} ${response.statusText}: ${await response.text()}`)
 
-  const transfers: Transfer[] = await response.json()
+  const transfers: StoredTransfer[] = await response.json()
 
   return transfers
 }
