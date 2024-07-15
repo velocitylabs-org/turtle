0018ca7452b0a4b29a4db6253ee072e18dd64561
Victor-Salomon <victor-salomon@hotmail.fr>
2024-07-15 16:23:13 +0200

    refacto ens fetch on transfer component

--- app/src/hooks/useLookupName.tsx
+++ app/src/hooks/useLookupName.tsx
@@ -1,17 +1,24 @@
-import { Network } from '@/models/chain'
-import { lookupName } from '@/utils/transfer'
 import { useCallback, useEffect, useState } from 'react'
 
-const useLookupName = (network: Network, address: string) => {
-  const [accountName, setAccountName] = useState<string | null>()
+import { Network } from '@/models/chain'
+import { lookupName } from '@/utils/transfer'
+
+const useLookupName = (network: Network | null, address: string | null) => {
+  const [accountName, setAccountName] = useState<string | null>(null)
 
   const fetchName = useCallback(async () => {
+    if (!network || !address) {
+      setAccountName(null)
+      return
+    }
+
     try {
       const name = await lookupName(network, address)
       setAccountName(name)
     } catch (e) {
       // Do not throw an error here
-      console.error(e)
+      console.error('Address lookup error:', e)
+      setAccountName(null)
     }
   }, [network, address])
 
