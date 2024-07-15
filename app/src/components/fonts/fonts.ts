372a9f4c273b33aaa85272e392adf88a3661ae2c
Nuno Alexandre <hi@nunoalexandre.com>
2024-07-15 12:55:29 +0200

    fonts: Add Dazzed Bold

--- app/src/components/fonts/fonts.ts
+++ app/src/components/fonts/fonts.ts
@@ -1,7 +1,15 @@
 import localFont from 'next/font/local'
 
 export const dazzed = localFont({
-  src: './dazzed.woff2',
-  display: 'swap',
+  src: [
+    {
+      path: './dazzed.woff2',
+      weight: '400',
+    },
+    {
+      path: './dazzed-bold.woff2',
+      weight: '700',
+    },
+  ],
   variable: '--font-dazzed',
 })
