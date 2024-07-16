8693c3206cd84b8d63c327ff7ec4e287896d460d
Noah Joeris <noahjoeris@gmail.com>
2024-07-16 16:37:36 +0200

    wip

--- app/src/components/Dropdown.tsx
+++ app/src/components/Dropdown.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode, RefObject } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface DropdownProps {
   isOpen: boolean
@@ -7,15 +8,26 @@ interface DropdownProps {
 }
 
 const Dropdown: FC<DropdownProps> = ({ isOpen, dropdownRef, children }) => {
-  if (!isOpen) return null
-
   return (
-    <div
-      ref={dropdownRef}
-      className="absolute left-0 right-0 z-20 max-h-60 min-h-[6rem] translate-y-[-3.58rem] overflow-auto rounded-md border-1 border-turtle-level3 bg-white"
-    >
-      <ul className="flex flex-col gap-2 px-1 py-2">{children}</ul>
-    </div>
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          ref={dropdownRef}
+          initial={{ height: '3.6rem' }}
+          animate={{
+            height: 'auto',
+            transition: { type: 'spring', stiffness: 300, damping: 20, duration: 0.2 },
+          }}
+          exit={{
+            height: '3.6rem',
+            transition: { type: 'spring', stiffness: 300, damping: 20, duration: 0.002 },
+          }}
+          className="absolute left-0 right-0 top-0 z-20 overflow-hidden rounded-md border-1 border-turtle-level3 bg-white"
+        >
+          <ul className="flex flex-col gap-2 px-1 py-2">{children}</ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
