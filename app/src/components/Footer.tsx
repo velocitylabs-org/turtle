bad6301e304efa7a24feb3b591da2ece4f9b2fb6
Noah Joeris <noahjoeris@gmail.com>
2024-07-17 18:06:35 +0200

    add logos

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 flex w-full items-center justify-center gap-1 border-t-2 border-turtle-level5 bg-turtle-level4 py-2 text-xs">
       <span className="text-center">Made by Velocity Labs</span>
-      {/* TODO: replace velocity logo */}
-      <Image src={'/snowbridge-logo.svg'} alt="snowbridge logo" height={20} width={20} />
+      <Image src={'/velocity-logo.png'} alt="velocity labs logo" height={20} width={20} />
 
       <span className="text-center">Powered by Snowbridge</span>
       <Image src={'/snowbridge-logo.svg'} alt="snowbridge logo" height={20} width={20} />
