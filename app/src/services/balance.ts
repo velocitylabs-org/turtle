0605611949f8cf6b4b4842881b8e4d5f420a948d
Victor-Salomon <victor-salomon@hotmail.fr>
2024-07-18 11:55:17 +0200

    add usd fees token value

--- app/src/services/balance.ts
+++ app/src/services/balance.ts
@@ -1,11 +1,16 @@
-import { Token } from '@/models/token'
 import * as Snowbridge from '@snowbridge/api'
 import {
   assetErc20Balance,
   erc20TokenToAssetLocation,
   palletAssetsBalance,
 } from '@snowbridge/api/dist/assets'
+import { Token } from '@/models/token'
 import { toHuman } from '../utils/transfer'
+import { Network } from '@/models/chain'
+
+export interface TokensValue {
+  [network: string]: { usd: number }
+}
 
 export interface Erc20Balance {
   value: bigint
@@ -56,5 +61,20 @@ export const fetchEthereumBalance = async (
     decimals: token.decimals,
     symbol: token.symbol,
     formatted: toHuman(balance, token).toString(),
+  }
+}
+
+export const getFeesTokensUSDValue = async (networkToken: Network): Promise<TokensValue | null> => {
+  try {
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${networkToken.toLowerCase()}&vs_currencies=usd`
+    const options = { method: 'GET', headers: { accept: 'application/json' } }
+    const result = await fetch(url, options)
+    if (!result.ok) {
+      throw new Error('Failed to fetch tokens value')
+    }
+    return (await result.json()) as TokensValue
+  } catch (error) {
+    console.log('Token value fetch error:', error)
+    return null
   }
 }
