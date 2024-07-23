7e3c3c2fc7777844533b2223528ff3c7f56a61ef
Noah Joeris <noahjoeris@gmail.com>
2024-07-23 16:45:00 +0200

    add sentry profiling

--- app/instrumentation.ts
+++ app/instrumentation.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/nextjs'
+import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
@@ -6,6 +7,13 @@ export async function register() {
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       // Adjust this value in production, or use tracesSampler for greater control
       tracesSampleRate: 1,
+      // Profiling sample rate is relative to tracesSampleRate
+      profilesSampleRate: 1.0,
+
+      integrations: [
+        // Add profiling integration to list of integrations
+        nodeProfilingIntegration(),
+      ],
 
       // Setting this option to true will print useful information to the console while you're setting up Sentry.
       debug: false,
--- app/sentry.client.config.ts
+++ app/sentry.client.config.ts
@@ -11,6 +11,13 @@ Sentry.init({
   // For example, to send 20% of transactions, set tracesSampleRate to 0.2
   tracesSampleRate: 1,
 
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate,
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // results in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
@@ -22,14 +29,15 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
+    // Add browser profiling integration to the list of integrations
+    Sentry.browserProfilingIntegration(),
+
     Sentry.replayIntegration({
-      // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
     }),
 
     Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
       colorScheme: 'light',
     }),
   ],
