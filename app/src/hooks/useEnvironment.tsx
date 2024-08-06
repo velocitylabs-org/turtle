23df3bab212a4b293f32d3815d66370a3399bf9b
Victor-Salomon <victor-salomon@hotmail.fr>
2024-08-06 10:24:35 +0200

    removed unsued code

--- app/src/hooks/useEnvironment.tsx
+++ app/src/hooks/useEnvironment.tsx
@@ -2,11 +2,6 @@ import { Environment } from '@/store/environmentStore'
 import { shouldUseTestnet } from '@/utils/env'
 
 const useEnvironment = () => {
-  // Kept this for when we will allow a user to switch between mainnet and testnet.
-
-  // const environment = useEnvironmentStore(state => state.current)
-  // const switchTo = useEnvironmentStore(state => state.switchTo)
-
   const environment = shouldUseTestnet ? Environment.Testnet : Environment.Mainnet
   return { environment }
 }
