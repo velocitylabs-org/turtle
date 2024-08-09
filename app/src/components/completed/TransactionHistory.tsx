fa7b276314e54ab6cd1a17c044f2b5a54ecb7101
Victor-Salomon <victor-salomon@hotmail.fr>
2024-08-09 16:16:09 +0200

    fix/invalid date on first load

--- app/src/components/completed/TransactionHistory.tsx
+++ app/src/components/completed/TransactionHistory.tsx
@@ -5,7 +5,13 @@ import { TransactionDialog } from './TransactionDialog'
 
 const TransactionHistory = ({ transactions }: { transactions: CompletedTransfer[] }) => {
   const transactionsByDate = transactions.reduce<TransfersByDate>((acc, transaction) => {
-    const date = transaction.date.toString().split('T')[0]
+    let date: string
+    if (typeof transaction.date === 'string') {
+      date = new Date(transaction.date).toISOString().split('T')[0]
+    } else {
+      date = transaction.date.toISOString().split('T')[0]
+    }
+
     if (!acc[date]) {
       acc[date] = []
     }
