3e97ffef031212e0b7ceafc6c3c60f294a6d7d43
Victor-Salomon <victor-salomon@hotmail.fr>
2024-08-12 17:14:48 +0200

    improve error handlers content

--- app/src/app/error.tsx
+++ app/src/app/error.tsx
@@ -27,15 +27,13 @@ const Error = ({ error }: { error: Error & { digest?: string } }) => {
       {/* Content */}
       <section className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center">
         <div className="flex flex-col items-center justify-center gap-8 text-white">
-          <h1 className="text-xl font-bold leading-5"> {/* Should be: 500 */}404</h1>
+          <h1 className="text-xl font-bold leading-5">500</h1>
           <h2 className="text-4xl font-medium leading-[56px] tracking-tighter sm:text-[56px]">
-            {/* Should be: Oops, something went wrong.*/} Oops, this page is missing.
+            Oops, something went wrong.
           </h2>
           <div className="flex flex-col justify-center gap-6 text-xl leading-5">
-            {/* Should not be there: */}
-            <p>Maybe you clicked on an old link!</p>
             <Link href="/" className="underline">
-              {/* Should not be: try again*/} Back home
+              Try again
             </Link>
           </div>
         </div>
--- app/src/app/global-error.tsx
+++ app/src/app/global-error.tsx
@@ -2,7 +2,6 @@
 import { captureException } from '@sentry/nextjs'
 import { useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 import Navbar from '@/components/NavBar'
 
 const GlobalError = ({ error }: { error: Error & { digest?: string } }) => {
@@ -34,18 +33,11 @@ const GlobalError = ({ error }: { error: Error & { digest?: string } }) => {
         {/* Content */}
         <section className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center">
           <div className="flex flex-col items-center justify-center gap-8 text-white">
-            <h1 className="text-xl font-bold leading-5"> {/* Should be: 500 */}404</h1>
+            <h1 className="text-xl font-bold leading-5">500</h1>
             <h2 className="text-4xl font-medium leading-[56px] tracking-tighter sm:text-[56px]">
-              {/* Should be: Oops, Turtle is temporary unavailable or eventually Oops, something went wrong.*/}{' '}
-              Oops, this page is missing.
+              Turtle is temporary unavailable.
             </h2>
-            <div className="flex flex-col justify-center gap-6 text-xl leading-5">
-              {/* Should not be there: */}
-              <p>Maybe you clicked on an old link!</p>
-              <Link href="/" className="underline">
-                {/* Should not be: try again later*/} Back home
-              </Link>
-            </div>
+            <div className="text-xl leading-5 underline">Try again later</div>
           </div>
         </section>
       </body>
