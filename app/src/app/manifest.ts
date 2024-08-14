5998f96a3878322c533dc6f29e7b1ceaa4835ff7
Victor-Salomon <victor-salomon@hotmail.fr>
2024-08-14 15:15:09 +0200

    add chrome images

--- app/src/app/manifest.ts
+++ app/src/app/manifest.ts
@@ -11,12 +11,22 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
-    categories: ['blockchain', 'web3', 'polkadot', 'defi', 'ethereum'],
+    categories: ['polkadot', 'ethereum', 'defi', 'blockchain', 'web3'],
     icons: [
       {
         src: '/favicon.ico',
         sizes: '32x32',
         type: 'image/x-icon',
+      },
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   }
