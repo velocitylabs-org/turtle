5336e6def49d23c4ee2d06ed2c617bb42559e222
Victor Salomon <68683271+Victor-Salomon@users.noreply.github.com>
2024-08-23 16:35:58 +0200

    update chain filters (#110)
    Co-authored-by: Noah Joeris <noahjoeris@gmail.com>
    Co-authored-by: Noah Joeris <33572822+noahjoeris@users.noreply.github.com>

--- app/src/utils/filters.ts
+++ app/src/utils/filters.ts
@@ -3,34 +3,65 @@ import { Chain } from '@/models/chain'
 import { Token } from '@/models/token'
 import { Environment } from '@/store/environmentStore'
 
-/** Filters all chains by compatibility for the selected destination and token. */
-// TODO
-export const getFilteredSourceChains = (
-  env: Environment,
-  _destinationChain: Chain | null,
-  _token: Token | null,
-) => {
-  return REGISTRY[env].chains
+/** Filters all chains by available routes. */
+export const getAllowedSourceChains = (env: Environment): (Chain & { allowed: boolean })[] => {
+  const routes = REGISTRY[env].routes
+
+  const chains = REGISTRY[env].chains.map(chain => {
+    const isAllowed = routes.some(route => route.from === chain.uid)
+
+    return {
+      ...chain,
+      allowed: isAllowed,
+    }
+  })
+
+  return chains
 }
 
-/** Filters all chains by compatibility for the selected source and token. */
-// TODO
-export const getFilteredDestinationChains = (
+/** Filters all chains by selected source chain, selected token and available routes */
+export const getAllowedDestinationChains = (
   env: Environment,
-  _sourceChain: Chain | null,
-  _token: Token | null,
-) => {
-  return REGISTRY[env].chains
+  sourceChain: Chain | null,
+  token: Token | null,
+): (Chain & { allowed: boolean })[] => {
+  const routes = REGISTRY[env].routes
+
+  return REGISTRY[env].chains.map(chain => {
+    const isAllowed =
+      sourceChain && token
+        ? routes.some(
+            route =>
+              route.from === sourceChain.uid &&
+              route.tokens.includes(token.id) &&
+              route.to === chain.uid,
+          )
+        : false
+
+    return {
+      ...chain,
+      allowed: isAllowed,
+    }
+  })
 }
 
-/** Filters all tokens by compatibility for the selected source and destination chain. */
-// TODO
-export const getFilteredTokens = (
+/** Filters all tokens by by selected source chain and available routes */
+export const getAllowedTokens = (
   env: Environment,
-  _sourceChain: Chain | null,
-  _destinationChain: Chain | null,
-) => {
-  const tokens = REGISTRY[env].tokens
+  sourceChain: Chain | null,
+): (Token & { allowed: boolean })[] => {
+  const routes = REGISTRY[env].routes
+
+  const tokens = REGISTRY[env].tokens.map(token => {
+    const isAllowed = sourceChain
+      ? routes.some(route => route.from === sourceChain.uid && route.tokens.includes(token.id))
+      : false
+
+    return {
+      ...token,
+      allowed: isAllowed,
+    }
+  })
 
   return tokens
 }
