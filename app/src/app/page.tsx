04a54190fa0084f542a72a120350e80a93e8cb35
Victor-Salomon <victor-salomon@hotmail.fr>
2024-09-03 16:56:45 +0200

    WIP getBridgeHubData

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,12 +5,14 @@ import { TurtlesBackground } from '@/components/TurtlesBackground'
 import { getEnvironment } from '@/context/snowbridge'
 import { shouldUseTestnet } from '@/utils/env'
 // WIP - TEST
-import { getHistoryTest } from '@/utils/subscan'
+import { getAhToParachainHistory } from '@/utils/subscan'
 import { Environment } from '@/store/environmentStore'
 
 const App: FC = async () => {
   const env = getEnvironment(shouldUseTestnet ? Environment.Testnet : Environment.Mainnet)
-  console.log(await getHistoryTest(env))
+  const txHash = '0xcdefd199b2b88c8ae43130dd4e48d8a5d3407556edfa617dba259bd51505a588'
+  const data = await getAhToParachainHistory(env, txHash)
+  console.log('AhToParachainHistory', data)
 
   return (
     <main>
--- app/src/utils/subscan.ts
+++ app/src/utils/subscan.ts
@@ -1,8 +1,18 @@
-import { AlchemyProvider } from 'ethers'
 import { environment, subscan } from '@snowbridge/api'
-import { BeefyClient__factory, IGateway__factory } from '@snowbridge/contract-types'
-import { hexToU8a, stringToU8a, u8aToHex } from '@polkadot/util'
-import { blake2AsU8a } from '@polkadot/util-crypto'
+import { bnToU8a, hexToU8a, stringToU8a, u8aToHex } from '@polkadot/util'
+import { blake2AsU8a, keccak256AsU8a } from '@polkadot/util-crypto'
+
+type messagequeueProcessed = {
+  id: number
+  block_timestamp: number
+  event_index: string
+  extrinsic_index: string
+  phase: number
+  module_id: string
+  event_id: string
+  extrinsic_hash: string
+  finalized: boolean
+}
 
 export const forwardedTopicId = (messageId: string): string => {
   const typeEncoded = stringToU8a('forward_id_for')
@@ -12,33 +22,20 @@ export const forwardedTopicId = (messageId: string): string => {
   return u8aToHex(newTopicId)
 }
 
-export async function getData(env: environment.SnowbridgeEnvironment, hash: string) {
-  if (!env.config.SUBSCAN_API) {
-    console.warn(`No subscan api urls configured for ${env.name}`)
-    // return []
-    throw new Error(`No subscan api urls configured for ${env.name}`)
-  }
-  const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY
-  if (!alchemyKey) {
-    throw Error('Missing Alchemy Key')
-  }
+export const paraIdToChannelId = (paraId: number): string => {
+  const typeEncoded = stringToU8a('para')
+  const paraIdEncoded = bnToU8a(paraId, { bitLength: 32, isLe: false })
+  const joined = new Uint8Array([...typeEncoded, ...paraIdEncoded])
+  const channelId = keccak256AsU8a(joined)
+  return u8aToHex(channelId)
+}
 
-  const subscanKey = process.env.NEXT_PUBLIC_SUBSCAN_KEY
-  if (!subscanKey) {
-    throw Error('Missing Subscan Key')
-  }
-
-  const assetHubScan = subscan.createApi(env.config.SUBSCAN_API.ASSET_HUB_URL, subscanKey)
-  const bridgeHubScan = subscan.createApi(env.config.SUBSCAN_API.BRIDGE_HUB_URL, subscanKey)
-  const relaychainScan = subscan.createApi(env.config.SUBSCAN_API.RELAY_CHAIN_URL, subscanKey)
-  const bridgeHubParaId = env.config.BRIDGE_HUB_PARAID
-  const assetHubParaId = env.config.ASSET_HUB_PARAID
-  const beacon_url = env.config.BEACON_HTTP_API
-  const ethChainId = env.ethChainId
-  const ethereumProvider = new AlchemyProvider(env.ethChainId, alchemyKey)
-  const beefyClient = BeefyClient__factory.connect(env.config.BEEFY_CONTRACT, ethereumProvider)
-  const gateway = IGateway__factory.connect(env.config.GATEWAY_CONTRACT, ethereumProvider)
-
+export async function getAssetHubData(
+  hash: string,
+  assetHubScan: subscan.SubscanApi,
+  relaychainScan: subscan.SubscanApi,
+  ethChainId: number,
+) {
   const subscanExtrinsicFetch = await assetHubScan.post('api/scan/extrinsic', {
     hash,
     only_extrinsic_event: true,
@@ -54,7 +51,6 @@ export async function getData(env: environment.SnowbridgeEnvironment, hash: stri
     extrinsic_index,
     extrinsic_hash,
   } = subscanExtrinsicFetch.json.data
-  // console.log(event)
 
   const xcmSentEvent = event.find(
     (e: any) => e.module_id === 'polkadotxcm' && e.event_id === 'Sent',
@@ -124,7 +120,6 @@ export async function getData(env: environment.SnowbridgeEnvironment, hash: stri
       continue
     }
 
-    // found first token
     break
   }
 
@@ -156,37 +151,98 @@ export async function getData(env: environment.SnowbridgeEnvironment, hash: stri
   }
 }
 
-export const getHistoryTest = async (env: environment.SnowbridgeEnvironment) => {
-  try {
-    const transfer = await getData(
-      env,
-      '0xcdefd199b2b88c8ae43130dd4e48d8a5d3407556edfa617dba259bd51505a588',
-    )
-    if (!transfer) return
+export async function getBridgeHubData(
+  bridgeHubScan: subscan.SubscanApi,
+  timestamp: number,
+  bridgeHubMessageId: string,
+) {
+  const eventIds = ['Processed', 'ProcessingFailed', 'OverweightEnqueued']
 
-    const result = {
-      id: transfer.data.messageId,
+  // implement an exponential retry mechanism
+  const fromBridgeHubBlock = await subscan.fetchBlockNearTimestamp(bridgeHubScan, timestamp)
+
+  const eventsBody = {
+    module: 'messagequeue',
+    block_range: `${fromBridgeHubBlock.block_num}-${fromBridgeHubBlock.block_num + 10}`,
+    event_id: eventIds.length === 1 ? eventIds[0] : undefined,
+    row: 100,
+    page: 0,
+  }
+
+  // paginate the query with a loop that handles the page param
+  const subscanEventFetch = await bridgeHubScan.post('api/v2/scan/events', eventsBody)
+
+  const events = await subscanEventFetch.json.data.events
+  if (subscanEventFetch.json.data?.count === 0 || events.length === 0) return []
+
+  const messageprocessedEvents = await Promise.all(
+    events.map(async (e: messagequeueProcessed) => {
+      const eventParams = await bridgeHubScan.post('api/scan/event/params', {
+        event_index: [e.event_index],
+      })
+      const { params } = eventParams.json.data[0]
+
+      const messageId = params.find((e: any) => e.name === 'id')?.value
+      if (!messageId || messageId !== bridgeHubMessageId) {
+        return
+      }
+
+      const origin = params.find((e: any) => e.name === 'origin')?.value
+      const sibling = origin?.Sibling ?? null
+      const channelId = origin?.Snowbridge ?? null
+      const success =
+        e.event_id === 'Processed' &&
+        (params.find((e: any) => e.name === 'success')?.value ?? false)
+
+      return { ...e, messageId, sibling, channelId, success }
+    }),
+  )
+  return messageprocessedEvents.filter(e => !!e)
+}
+
+export const getAhToParachainHistory = async (
+  env: environment.SnowbridgeEnvironment,
+  hash: string,
+) => {
+  if (!env.config.SUBSCAN_API) {
+    throw new Error(`No subscan api urls configured for ${env.name}`)
+  }
+  const subscanKey = process.env.NEXT_PUBLIC_SUBSCAN_KEY
+  if (!subscanKey) {
+    throw Error('Missing Subscan Key')
+  }
+
+  const assetHubScan = subscan.createApi(env.config.SUBSCAN_API.ASSET_HUB_URL, subscanKey)
+  const relaychainScan = subscan.createApi(env.config.SUBSCAN_API.RELAY_CHAIN_URL, subscanKey)
+  const bridgeHubScan = subscan.createApi(env.config.SUBSCAN_API.BRIDGE_HUB_URL, subscanKey)
+
+  try {
+    const ahTransferData = await getAssetHubData(hash, assetHubScan, relaychainScan, env.ethChainId)
+    if (!ahTransferData) return // TODO improve error
+
+    const result: any = {
+      id: ahTransferData.data.messageId,
       status: 0, // === pending
       info: {
-        when: new Date(transfer.data.block_timestamp * 1000),
-        sourceAddress: transfer.data.account_id,
-        tokenAddress: transfer.data.tokenAddress,
-        beneficiaryAddress: transfer.data.beneficiaryAddress,
-        amount: transfer.data.amount,
+        when: new Date(ahTransferData.data.block_timestamp * 1000),
+        sourceAddress: ahTransferData.data.account_id,
+        tokenAddress: ahTransferData.data.tokenAddress,
+        beneficiaryAddress: ahTransferData.data.beneficiaryAddress,
+        amount: ahTransferData.data.amount,
       },
       submitted: {
-        extrinsic_index: transfer.data.extrinsic_index,
-        extrinsic_hash: transfer.data.extrinsic_hash,
-        block_hash: transfer.data.block_hash,
-        account_id: transfer.data.account_id,
-        block_num: transfer.data.block_num,
-        block_timestamp: transfer.data.block_timestamp,
-        messageId: transfer.data.messageId,
-        bridgeHubMessageId: transfer.data.bridgeHubMessageId,
-        success: transfer.data.success,
+        extrinsic_index: ahTransferData.data.extrinsic_index,
+        extrinsic_hash: ahTransferData.data.extrinsic_hash,
+        block_hash: ahTransferData.data.block_hash,
+        account_id: ahTransferData.data.account_id,
+        block_num: ahTransferData.data.block_num,
+        block_timestamp: ahTransferData.data.block_timestamp,
+        messageId: ahTransferData.data.messageId,
+        bridgeHubMessageId: ahTransferData.data.bridgeHubMessageId,
+        success: ahTransferData.data.success,
         relayChain: {
-          block_num: transfer.data.relayChain.block_num,
-          block_hash: transfer.data.relayChain.block_hash,
+          block_num: ahTransferData.data.relayChain.block_num,
+          block_hash: ahTransferData.data.relayChain.block_hash,
         },
       },
     }
@@ -194,8 +250,51 @@ export const getHistoryTest = async (env: environment.SnowbridgeEnvironment) => 
     if (!result.submitted.success) {
       result.status = 2 // === failed
     }
+
+    if (!result.submitted.bridgeHubMessageId) {
+      return // TODO improve error
+    }
+
+    const bridgeHubXcmDelivered = await getBridgeHubData(
+      bridgeHubScan,
+      result.submitted.block_timestamp,
+      result.submitted.bridgeHubMessageId,
+    )
+
+    if (bridgeHubXcmDelivered.length) {
+      result.bridgeHubXcmDelivered = {
+        block_timestamp: bridgeHubXcmDelivered[0].block_timestamp,
+        event_index: bridgeHubXcmDelivered[0].event_index,
+        extrinsic_hash: bridgeHubXcmDelivered[0].extrinsic_hash,
+        siblingParachain: bridgeHubXcmDelivered[0].sibling,
+        success: bridgeHubXcmDelivered[0].success,
+      }
+      if (!result.bridgeHubXcmDelivered.success) {
+        result.status = 2 // === failed
+      }
+    }
     return result
   } catch (error) {
     console.log(error)
   }
 }
+
+// BEGINING TEST
+// const context = await getSnowBridgeContext()
+// const { api: bridgeHub } = await constructApiPromise(
+//   'wss://rococo-bridge-hub-rpc.polkadot.io',
+// )
+// try {
+//   const test = await waitForMessageQueuePallet(
+//     context.polkadot.api.assetHub,
+//     "0x96618413c7dd80a998d3247751c9557f889bfd9df7cd3d06bf4148b86a8be5c9",
+//     env.config.BRIDGE_HUB_PARAID,
+//     () => true,
+//     {
+//       scanBlocks: 40,
+//     }
+//   )
+// } catch (error) {
+//   console.log(error)
+// }
+// END TEST
