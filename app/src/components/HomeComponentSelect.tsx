3badb0dd41071204253ec48a76502ab85832d2df
Nuno <hi@nunoalexandre.com>
2024-09-12 12:53:21 +0200

    fix: Preserve Transfer form state on menu tab switch (#125)
    Co-authored-by: Noah Joeris <33572822+noahjoeris@users.noreply.github.com>

--- app/src/components/HomeComponentSelect.tsx
+++ app/src/components/HomeComponentSelect.tsx
@@ -9,6 +9,7 @@ import { TransferTabOptions, TransferTab } from '@/models/transfer'
 
 import OngoingTransfers from './OngoingTransfers'
 import TransactionLoaderSkeleton from './completed/TransactionLoaderSkeleton'
+import { cn } from '@/utils/cn'
 
 const TransferHistory = dynamic(() => import('@/components/completed/TransactionHistory'), {
   loading: () => <TransactionLoaderSkeleton />,
@@ -26,18 +27,23 @@ export const HomeComponentSelect = () => {
         setNewTransferInit={setNewTransferInit}
         hasCompletedTransfers={hasCompletedTransfers}
       />
-      {newTransferInit === TransferTab.New ? (
-        <div className="z-15 relative max-w-[90vw]">
-          <Transfer />
-          <OngoingTransfers
-            newTransferInit={newTransferInit}
-            setNewTransferInit={setNewTransferInit}
-            hasCompletedTransfers={hasCompletedTransfers}
-          />
-        </div>
-      ) : (
-        hasCompletedTransfers &&
-        completedTransfers && <TransferHistory transactions={completedTransfers} />
+
+      <div
+        className={cn(
+          'z-15 relative max-w-[90vw]',
+          newTransferInit !== TransferTab.New && 'hidden',
+        )}
+      >
+        <Transfer />
+        <OngoingTransfers
+          newTransferInit={newTransferInit}
+          setNewTransferInit={setNewTransferInit}
+          hasCompletedTransfers={hasCompletedTransfers}
+        />
+      </div>
+
+      {hasCompletedTransfers && completedTransfers && (
+        <TransferHistory transactions={completedTransfers!} />
       )}
     </>
   )
