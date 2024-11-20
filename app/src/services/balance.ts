2eeb9e54b9359672ea7c75b7024c08eba1f45716
Victor-Salomon <victor-salomon@hotmail.fr>
2024-11-20 10:18:47 +0100

    removed unsued fetchAssetHubBalance function

--- app/src/services/balance.ts
+++ app/src/services/balance.ts
@@ -1,7 +1,4 @@
 import { Token } from '@/models/token'
-import { Context } from '@snowbridge/api'
-import { erc20TokenToAssetLocation, palletAssetsBalance } from '@snowbridge/api/dist/assets'
-import { toHuman } from '../utils/transfer'
 
 export interface TokenPrice {
   usd: number
@@ -12,34 +9,6 @@ export interface Erc20Balance {
   decimals: number
   symbol: string
   formatted: string
-}
-
-export const fetchAssetHubBalance = async (
-  context: Context,
-  token: Token,
-  address: string,
-): Promise<Erc20Balance> => {
-  const chainId = (await context.ethereum.api.getNetwork()).chainId
-  const multiLocation = erc20TokenToAssetLocation(
-    context.polkadot.api.assetHub.registry,
-    BigInt(chainId),
-    token.address,
-  )
-
-  const balance = await palletAssetsBalance(
-    context.polkadot.api.assetHub,
-    multiLocation,
-    address,
-    'foreignAssets',
-  )
-
-  const fetchedBalance = {
-    value: balance ?? 0n,
-    decimals: token.decimals,
-    symbol: token.symbol,
-    formatted: toHuman(balance ?? 0n, token).toString(),
-  }
-  return fetchedBalance
 }
 
 export const getTokenPrice = async (tokenId: string): Promise<TokenPrice | null> => {
