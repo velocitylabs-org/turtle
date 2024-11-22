11c8b0105355191a0fd1e9a0bb6c97a123a947fa
Victor-Salomon <victor-salomon@hotmail.fr>
2024-11-22 12:01:52 +0100

    unlock commit

--- app/src/components/TxSummary.tsx
+++ app/src/components/TxSummary.tsx
@@ -35,6 +35,7 @@ const TxSummary: FC<TxSummaryProps> = ({ loading, tokenAmount, fees, durationEst
         </div>
       )
     }
+    console.log("test commit")
 
     return (
       <div className="tx-summary p-4 pt-3">
