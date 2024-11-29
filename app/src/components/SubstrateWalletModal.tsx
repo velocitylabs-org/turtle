7f2d7a248b16b4374a8af90ee88c025ee259b42f
Noah Joeris <noahjoeris@gmail.com>
2024-11-29 14:56:48 +0200

    fix issue

--- app/src/components/SubstrateWalletModal.tsx
+++ app/src/components/SubstrateWalletModal.tsx
@@ -1,8 +1,9 @@
 'use client'
 import useSubstrateWallet from '@/hooks/useSubstrateWallet'
-import { truncateAddress } from '@/utils/address'
 import { isMobileDevice } from '@/utils/env'
+import { getWalletLogo, getWalletName } from '@/utils/wallet'
 import type { InjectedAccount, InjectedExtension } from '@polkadot/extension-inject/types'
+import Image from 'next/image'
 import { FC, useEffect, useState } from 'react'
 import Button from './Button'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog'
@@ -60,13 +61,13 @@ const SubstrateWalletModal: FC = () => {
   return (
     <Dialog open={isModalOpen} onOpenChange={open => (open ? openModal() : closeModal())}>
       <DialogContent
-        className="m-auto max-h-[85vh] max-w-[90vw] overflow-scroll rounded-4xl sm:max-w-[30.5rem]"
+        className="m-auto max-h-[85vh] max-w-[25rem] overflow-scroll rounded-4xl pb-4"
         hideCloseButton={true}
       >
         {/* Header */}
-        <DialogHeader className="flex items-center justify-center rounded-t-[32px] border border-turtle-secondary-dark bg-turtle-secondary-light py-6">
-          <DialogTitle className="text-xl font-semibold text-turtle-secondary-dark">
-            {currentView === 'extensions' ? 'Select Wallet' : 'Select Account'}
+        <DialogHeader className="flex items-center justify-between rounded-t-4xl bg-turtle-level2 p-4">
+          <DialogTitle className="text-lg font-bold">
+            {currentView === 'extensions' ? 'Connect Wallet' : 'Connect Account'}
           </DialogTitle>
         </DialogHeader>
 
@@ -77,50 +78,73 @@ const SubstrateWalletModal: FC = () => {
               extensions.map(extension => (
                 <Button
                   key={extension.name}
-                  className="w-full p-4"
+                  className="flex w-full items-center justify-between rounded-md border bg-gray-50 p-4 hover:bg-gray-100"
                   variant="outline"
-                  label={extension.name}
                   onClick={() => handleExtensionSelect(extension)}
-                />
+                >
+                  <div className="flex items-center space-x-2">
+                    <Image
+                      src={getWalletLogo(extension.name)}
+                      alt={`${extension.name} Logo`}
+                      width={64}
+                      height={64}
+                      className="h-8 w-8"
+                    />
+                    <span>{getWalletName(extension.name)}</span>
+                  </div>
+                  <span className="text-sm text-turtle-primary-dark">Installed</span>
+                </Button>
               ))
             ) : (
               <p className="text-center text-sm text-gray-500">
-                No extensions detected. Please install a compatible wallet extension. For example,
+                No extensions detected. Please install a compatible wallet extension, such as
                 Talisman, Subwallet, or Polkadot.js.
               </p>
             ))}
-
-          {currentView === 'accounts' &&
-            (extensionAccounts.length > 0 ? (
-              extensionAccounts
-                .filter(account =>
-                  type === 'SubstrateEVM'
-                    ? account.type === 'ethereum'
-                    : account.type === 'sr25519',
-                )
-                .map(account => (
-                  <Button
-                    key={account.address}
-                    className="flex w-full flex-col"
-                    variant="outline"
-                    onClick={() => handleAccountSelect(account)}
-                  >
-                    <span className="pt-3">{account.name}</span>
-                    <span className="pb-4 text-xs text-turtle-level6">
-                      {truncateAddress(account.address)}
-                    </span>
-                  </Button>
-                ))
-            ) : (
-              <p className="text-center text-sm text-gray-500">
-                No accounts available. Please connect an account to Turtle inside your wallet
-                extension.
-              </p>
-            ))}
         </div>
+
+        {/* Footer */}
+        {currentView === 'extensions' && (
+          <div className="mt-4 text-center text-sm text-gray-500">
+            Haven&apos;t got a wallet?{' '}
+            <a
+              href="https://support.polkadot.network/support/solutions/articles/65000098878-how-to-create-a-polkadot-account"
+              className="text-blue-500"
+            >
+              Get started
+            </a>
+          </div>
+        )}
       </DialogContent>
     </Dialog>
   )
 }
 
 export default SubstrateWalletModal
+
+/* {
+  currentView === 'accounts' &&
+    (extensionAccounts.length > 0 ? (
+      extensionAccounts
+        .filter(account =>
+          type === 'SubstrateEVM' ? account.type === 'ethereum' : account.type === 'sr25519',
+        )
+        .map(account => (
+          <Button
+            key={account.address}
+            className="flex w-full flex-col"
+            variant="outline"
+            onClick={() => handleAccountSelect(account)}
+          >
+            <span className="pt-3">{account.name}</span>
+            <span className="pb-4 text-xs text-turtle-level6">
+              {truncateAddress(account.address)}
+            </span>
+          </Button>
+        ))
+    ) : (
+      <p className="text-center text-sm text-gray-500">
+        No accounts available. Please connect an account to Turtle inside your wallet extension.
+      </p>
+    ))
+} */
---
+++ app/src/utils/wallet.ts
@@ -0,0 +1,34 @@
+export const capitalizeFirstLetter = (text: string): string =>
+  text.charAt(0).toUpperCase() + text.slice(1)
+
+export const getWalletName = (name: string): string => {
+  switch (name) {
+    case 'talisman':
+      return 'Talisman'
+
+    case 'subwallet-js':
+      return 'SubWallet'
+
+    case 'polkadot-js':
+      return 'Polkadot.js'
+
+    default:
+      return capitalizeFirstLetter(name)
+  }
+}
+
+export const getWalletLogo = (name: string): string => {
+  switch (name) {
+    case 'talisman':
+      return '/talisman.png'
+
+    case 'subwallet-js':
+      return '/subwallet.svg'
+
+    case 'polkadot-js':
+      return '/polkadotjs.svg'
+
+    default:
+      return '/wallet.svg'
+  }
+}
