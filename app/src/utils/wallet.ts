8a05e13362cb182616cff1adfdf50973260bf6b9
Victor-Salomon <victor-salomon@hotmail.fr>
2024-12-09 14:20:16 +0100

    add nova-wallet logic

--- app/src/utils/wallet.ts
+++ app/src/utils/wallet.ts
@@ -1,7 +1,12 @@
 export const capitalizeFirstLetter = (text: string): string =>
   text.charAt(0).toUpperCase() + text.slice(1)
 
-export const getWalletName = (name: string): string => {
+const isNovaWallet = (window?: Window): boolean => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return !!(window && (window as any).walletExtension?.isNovaWallet === true)
+}
+
+export const getWalletName = (name: string, window?: Window): string => {
   switch (name) {
     case 'talisman':
       return 'Talisman'
@@ -9,8 +14,10 @@ export const getWalletName = (name: string): string => {
     case 'subwallet-js':
       return 'SubWallet'
 
-    case 'polkadot-js':
+    case 'polkadot-js': {
+      if (isNovaWallet(window)) return 'Nova Wallet'
       return 'Polkadot.js'
+    }
 
     case 'fearless-wallet':
       return 'Fearless Wallet'
@@ -20,7 +27,7 @@ export const getWalletName = (name: string): string => {
   }
 }
 
-export const getWalletLogo = (name: string): string => {
+export const getWalletLogo = (name: string, window?: Window): string => {
   switch (name) {
     case 'talisman':
       return '/logos/talisman.svg'
@@ -28,8 +35,10 @@ export const getWalletLogo = (name: string): string => {
     case 'subwallet-js':
       return '/logos/subwallet.svg'
 
-    case 'polkadot-js':
+    case 'polkadot-js': {
+      if (isNovaWallet(window)) return '/logos/novawallet.svg'
       return '/logos/polkadotjs.svg'
+    }
 
     case 'fearless-wallet':
       return '/logos/fearless.svg'
