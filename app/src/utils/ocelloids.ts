88eb3398298098446426a6e83ac32daef20ab28a
Victor-Salomon <victor-salomon@hotmail.fr>
2024-12-18 15:35:53 +0100

    implement transfer handling in ocelloids

--- app/src/utils/ocelloids.ts
+++ app/src/utils/ocelloids.ts
@@ -1,5 +1,7 @@
-import { StoredTransfer } from '@/models/transfer'
+import { CompletedTransfer, StoredTransfer, TxStatus } from '@/models/transfer'
 import { OcelloidsAgentApi, OcelloidsClient, xcm } from '@sodazone/ocelloids-client'
+import { getExplorerLink } from './transfer'
+import { NotificationSeverity, Notification } from '@/models/notification'
 
 const apiKey = process.env.NEXT_PUBLIC_OC_API_KEY_READ_WRITE || ''
 
@@ -15,7 +17,7 @@ export const getOcelloidsAgentApi = async (): Promise<
     const OCLD_ClIENT = initOcelloidsClient()
 
     await OCLD_ClIENT.health()
-      .then(() => {})
+      .then(() => { })
       .catch(error => {
         console.error('Occeloids health error', error)
         throw new Error('Occeloids health failed')
@@ -27,13 +29,39 @@ export const getOcelloidsAgentApi = async (): Promise<
   }
 }
 
+type ResultData = {
+  message: string
+  severity: NotificationSeverity
+  status: TxStatus
+}
+
+const getSubscription = (
+  sourceChainId: number,
+  destChainId: number,
+  sender?: string,
+  events?: xcm.XcmNotificationType[],
+): xcm.XcmInputs => {
+  const consensus = 'polkadot'
+  return {
+    origin: `urn:ocn:${consensus}:${sourceChainId}`,
+    senders: sender ? [sender] : '*',
+    events: events ? events : '*',
+    destinations: [`urn:ocn:${consensus}:${destChainId}`],
+  }
+}
+
 export const xcmOcceloidsSubscribe = async (
   ocelloidsAgentApi: OcelloidsAgentApi<xcm.XcmInputs>,
   transfer: StoredTransfer,
+  remove: (id: string) => void,
+  addCompletedTransfer: (completedTransfer: CompletedTransfer) => void,
+  addNotification: (notification: Omit<Notification, 'id'>) => void,
 ) => {
   try {
     if (!ocelloidsAgentApi) throw new Error('Occeloids Agent is undefined')
     const { id: txHash, sourceChain, destChain } = transfer
+    const explorerLink = getExplorerLink(transfer)
+
     const ws = await ocelloidsAgentApi.subscribe<xcm.XcmMessagePayload>(
       getSubscription(sourceChain.chainId, destChain.chainId),
       {
@@ -42,21 +70,45 @@ export const xcmOcceloidsSubscribe = async (
           if (payload.origin.extrinsicHash === txHash) {
             // Handle different XCM event types
             switch (payload.type) {
-              case 'xcm.sent':
+              case xcm.XcmNotificationType.Sent:
                 console.log('SENT', payload)
                 break
-              case 'xcm.relayed':
+              case xcm.XcmNotificationType.Relayed:
                 console.log('RELAYED', payload)
                 break
-              case 'xcm.hop':
+              case xcm.XcmNotificationType.Hop:
                 console.log('HOP - TRANSFER FAILED', payload)
+                handleTransferRecord(
+                  transfer,
+                  remove,
+                  addCompletedTransfer,
+                  addNotification,
+                  xcm.XcmNotificationType.Hop,
+                  explorerLink,
+                )
                 break
-              case 'xcm.received':
+              case xcm.XcmNotificationType.Received:
                 console.log('RECEIVED - TRANSFER RECEIVED', payload)
+                handleTransferRecord(
+                  transfer,
+                  remove,
+                  addCompletedTransfer,
+                  addNotification,
+                  xcm.XcmNotificationType.Received,
+                  explorerLink,
+                )
                 ws.close()
                 break
-              case 'xcm.timeout':
+              case xcm.XcmNotificationType.Timeout:
                 console.log('TIMEOUT - TRANSFER FAILED', payload)
+                handleTransferRecord(
+                  transfer,
+                  remove,
+                  addCompletedTransfer,
+                  addNotification,
+                  xcm.XcmNotificationType.Timeout,
+                  explorerLink,
+                )
                 ws.close()
                 break
               default:
@@ -69,7 +121,7 @@ export const xcmOcceloidsSubscribe = async (
         onClose: event => console.log('WebSocket Closed', event.reason),
       },
       {
-        onSubscriptionCreated: () => {},
+        onSubscriptionCreated: () => { },
         onSubscriptionError: console.error,
         onError: console.error,
       },
@@ -79,19 +131,61 @@ export const xcmOcceloidsSubscribe = async (
   }
 }
 
-type Consensus = 'polkadot'
+const handleTransferRecord = (
+  transfer: StoredTransfer,
+  remove: (id: string) => void,
+  addCompletedTransfer: (completedTransfer: CompletedTransfer) => void,
+  addNotification: (notification: Omit<Notification, 'id'>) => void,
+  xcmMsgType: xcm.XcmNotificationType,
+  explorerLink?: string,
+) => {
+  const { status, message, severity } = getResultData(xcmMsgType)
 
-const getSubscription = (
-  sourceChainId: number,
-  destChainId: number,
-  sender?: string,
-  events?: xcm.XcmNotificationType[],
-  consensus: Consensus = 'polkadot',
-): xcm.XcmInputs => {
-  return {
-    origin: `urn:ocn:${consensus}:${sourceChainId}`,
-    senders: sender ? [sender] : '*',
-    events: events ? events : '*',
-    destinations: [`urn:ocn:${consensus}:${destChainId}`],
+  remove(transfer.id)
+
+  addCompletedTransfer({
+    id: transfer.id,
+    result: status,
+    token: transfer.token,
+    sourceChain: transfer.sourceChain,
+    destChain: transfer.destChain,
+    amount: transfer.amount,
+    tokenUSDValue: transfer.tokenUSDValue ?? 0,
+    fees: transfer.fees,
+    sender: transfer.sender,
+    recipient: transfer.recipient,
+    date: transfer.date,
+    ...(explorerLink && { explorerLink }),
+  } satisfies CompletedTransfer)
+
+  addNotification({
+    message,
+    severity,
+    dismissible: true,
+  })
+}
+
+const getResultData = (xcmMsgType: xcm.XcmNotificationType): ResultData => {
+  switch (xcmMsgType) {
+    case xcm.XcmNotificationType.Hop:
+      return {
+        message: 'Transfer failed!',
+        severity: NotificationSeverity.Error,
+        status: TxStatus.Failed,
+      }
+
+    case xcm.XcmNotificationType.Timeout:
+      return {
+        message: 'Transfer timed out!',
+        severity: NotificationSeverity.Warning,
+        status: TxStatus.Undefined,
+      }
+
+    default:
+      return {
+        message: 'Transfer completed!',
+        severity: NotificationSeverity.Success,
+        status: TxStatus.Succeeded,
+      }
   }
 }
