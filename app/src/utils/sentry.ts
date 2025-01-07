d1ed759722b50eac6616d59d6880ddb1b1ff327b
Victor-Salomon <victor-salomon@hotmail.fr>
2025-01-07 17:25:16 +0100

    wip sentry file

---
+++ app/src/utils/sentry.ts
@@ -0,0 +1,11 @@
+// import { captureException, SeverityLevel } from '@sentry/nextjs'
+
+// function captureCustomException<T = unknown>(error: any, level?: SeverityLevel, data?: T) {
+//     captureException(error, {
+//         level,
+//         tags: {
+//             environment: "production",
+//         },
+//         extra: { data }
+//     });
+// }
