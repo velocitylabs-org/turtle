213f1f0ea274bae3cbfef2efbc2d9ee8845d7db7
Victor-Salomon <victor-salomon@hotmail.fr>
2025-01-07 16:36:48 +0100

    Final TransactionHistory height

--- app/src/components/completed/TransactionHistory.tsx
+++ app/src/components/completed/TransactionHistory.tsx
@@ -8,7 +8,7 @@ import { formatTransfersByDate } from '@/utils/transfer'
 const TransactionHistory = ({ transfers }: { transfers: CompletedTransfer[] }) => {
   const formattedTransfers = formatTransfersByDate(transfers)
   return (
-    <div className="z-20 mb-12 flex max-h-[536px] max-w-[90vw] flex-col gap-4 overflow-auto rounded-3xl border-1 border-turtle-foreground bg-white px-[1.5rem] py-[2rem] sm:w-[31.5rem] sm:p-[2.5rem]">
+    <div className="z-20 mb-12 flex max-h-[70vh] max-w-[90vw] flex-col gap-4 overflow-auto rounded-3xl border-1 border-turtle-foreground bg-white px-[1.5rem] py-[2rem] sm:w-[31.5rem] sm:p-[2.5rem]">
       {formattedTransfers.map(({ date, transfers }, idx) => (
         <div key={idx + date + transfers.length}>
           <div className="w-full space-y-3">
