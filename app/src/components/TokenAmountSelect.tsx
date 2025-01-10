2d2d5c461aedef06306ace83ae4212b1b20faef9
Noah Joeris <33572822+noahjoeris@users.noreply.github.com>
2025-01-10 17:13:37 +0100

    improvement: add compact notation to usd value (#251)

--- app/src/components/TokenAmountSelect.tsx
+++ app/src/components/TokenAmountSelect.tsx
@@ -1,18 +1,20 @@
 'use client'
 import { useOutsideClick } from '@/hooks/useOutsideClick'
+import useTokenPrice from '@/hooks/useTokenPrice'
+import { Chain } from '@/models/chain'
 import { SelectProps, TokenAmount } from '@/models/select'
 import { Token } from '@/models/token'
 import { cn } from '@/utils/cn'
+import NumberFlow from '@number-flow/react'
 import { forwardRef, useRef, useState } from 'react'
 import Dropdown from './Dropdown'
 import ChevronDown from './svg/ChevronDown'
 import TokenIcon from './svg/TokenIcon'
+import { TokenLogo } from './TokenLogo'
 import { Tooltip } from './Tooltip'
 import VerticalDivider from './VerticalDivider'
-import { TokenLogo } from './TokenLogo'
-import { Chain } from '@/models/chain'
-import NumberFlow from '@number-flow/react'
-import useTokenPrice from '@/hooks/useTokenPrice'
+
+const maxDollars = 100000000000 // 100B
 
 export interface TokenAmountSelectProps extends SelectProps<TokenAmount> {
   sourceChain: Chain | null
@@ -123,7 +125,14 @@ const TokenAmountSelect = forwardRef<HTMLDivElement, TokenAmountSelectProps>(
                 />
                 {inDollars && (
                   <div className={'animate-slide-up mt-[-3px] text-sm text-turtle-level4'}>
-                    <NumberFlow value={inDollars} prefix="$" />
+                    <NumberFlow
+                      value={Math.min(inDollars, maxDollars)} // Ensure the value doesn't exceed the max
+                      prefix="$"
+                      format={{
+                        notation: 'compact',
+                        maximumFractionDigits: 3,
+                      }}
+                    />
                   </div>
                 )}
               </div>
