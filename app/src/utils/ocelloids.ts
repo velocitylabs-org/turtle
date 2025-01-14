f18c01dba8cbf9ce786cfa9a85d2cca9a01e0742
Victor Salomon <68683271+Victor-Salomon@users.noreply.github.com>
2025-01-14 14:15:23 +0100

    Update app/src/utils/ocelloids.ts
    Co-authored-by: Noah Joeris <33572822+noahjoeris@users.noreply.github.com>

--- app/src/utils/ocelloids.ts
+++ app/src/utils/ocelloids.ts
@@ -41,7 +41,7 @@ export const getSubscribableTransfers = (transfers: StoredTransfer[]) => {
 }
 
 export const initOcelloidsClient = () => {
-  if (!OCELLOIDS_API_KEY || !OCELLOIDS_API_KEY.length)
+  if (!OCELLOIDS_API_KEY)
     throw new Error('OCELLOIDS_API_KEY is undefined')
   return new OcelloidsClient({
     apiKey: OCELLOIDS_API_KEY,
