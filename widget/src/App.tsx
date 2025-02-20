539e7969abdcdd042f7a7d1776c2ea7640bbfee7
Victor-Salomon <victor-salomon@hotmail.fr>
2025-02-20 17:15:34 +0100

    add hero-ui

--- widget/src/App.tsx
+++ widget/src/App.tsx
@@ -1,5 +1,10 @@
+import { HeroUIProvider } from '@heroui/react'
 function App() {
-  return <>Turtle Widget</>
+  return (
+    <HeroUIProvider>
+      <>Turtle Widget</>
+    </HeroUIProvider>
+  )
 }
 
 export default App
