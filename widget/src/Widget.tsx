f64d6c89c092d933e8a9d00deea51c5c9c3f9cb4
Victor-Salomon <victor-salomon@hotmail.fr>
2025-02-25 15:37:32 +0100

    add transfer logic and outsource reownappkit config

--- widget/src/Widget.tsx
+++ widget/src/Widget.tsx
@@ -1,21 +1,15 @@
-import WalletButton from './components/WalletButton'
-import useWallet from './hooks/useWallet'
-import { truncateAddress } from './utils/address'
+import Transfer from './components/Transfer'
 
 export interface WidgetProps {
   title?: string
 }
 
 export const Widget: React.FC<WidgetProps> = ({ title = 'Transfers Widget' }) => {
-  const sourceWallet = useWallet('SubstrateEVM')
   return (
     <div className="bg-card m-4 mx-auto max-w-sm rounded-lg p-6 text-center shadow-md">
       <h2 className="text-card-foreground mb-4 text-2xl font-bold">{title}</h2>
-      <div>
-        {sourceWallet?.sender?.address && truncateAddress(sourceWallet?.sender?.address, 10)}
-      </div>
       <div className="space-x-2">
-        <WalletButton walletType="SubstrateEVM" />
+        <Transfer />
       </div>
     </div>
   )
