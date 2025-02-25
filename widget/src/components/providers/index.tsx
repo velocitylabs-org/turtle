4d454706dcf3fdf6dee1a4be852886223d434a3b
Victor-Salomon <victor-salomon@hotmail.fr>
2025-02-25 15:58:08 +0100

    removed duplicated content

--- widget/src/components/providers/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import HeroUiProvider from './HeroUiProvider'
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUiProvider>{children}</HeroUiProvider>
-}
