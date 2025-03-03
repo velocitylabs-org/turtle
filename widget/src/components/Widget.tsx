dac916c614aef008f6e09791bd014085b51d7b12
Victor-Salomon <victor-salomon@hotmail.fr>
2025-03-03 15:43:43 +0100

    wip fix build

--- widget/src/components/Widget.tsx
+++ widget/src/components/Widget.tsx
@@ -1,6 +1,7 @@
 import NotificationSystem from '@/components/NotificationSystem'
 import SubstrateWalletModal from '@/components/SubstrateWalletModal'
 import Transfer from '@/components/Transfer'
+import { Providers } from '@/providers'
 
 export interface WidgetProps {
   title?: string
@@ -8,13 +9,15 @@ export interface WidgetProps {
 
 export const Widget: React.FC<WidgetProps> = ({ title = 'Transfers Widget' }) => {
   return (
-    <div className="m-4 flex flex-col items-center justify-center p-6">
-      <h2 className="text-card-foreground mb-4 text-2xl font-bold">{title}</h2>
-      <div>
-        <Transfer />
+    <Providers>
+      <div className="m-4 flex flex-col items-center justify-center p-6">
+        <h2 className="text-card-foreground mb-4 text-2xl font-bold">{title}</h2>
+        <div>
+          <Transfer />
+        </div>
+        <NotificationSystem />
+        <SubstrateWalletModal />
       </div>
-      <NotificationSystem />
-      <SubstrateWalletModal />
-    </div>
+    </Providers>
   )
 }
--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -2,15 +2,39 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import tailwindcss from 'tailwindcss'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+// import copy from 'rollup-plugin-copy'
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
       tsconfigPath: 'tsconfig.app.json',
-      rollupTypes: true,
+      outDir: 'dist', // put .d.ts files in ./dist
+      insertTypesEntry: true, // create an index.d.ts entry
+      // rollupTypes: true,
     }),
+    cssInjectedByJsPlugin(),
+    // {
+    //   ...copy({
+    //     targets: [
+    //       // Copy entire subfolders from src/assets
+    //       { src: 'src/assets/fonts', dest: 'dist/src/assets' },
+    //       { src: 'src/assets/logos', dest: 'dist/src/assets' },
+    //       { src: 'src/assets/svg', dest: 'dist/src/assets' },
+    //       { src: 'src/assets/wallets-logo', dest: 'dist/src/assets' },
+    //     ],
+    //     hook: 'writeBundle',
+    //   }),
+    //   apply: 'build',
+    // },
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -19,18 +43,20 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'turtle-widget',
-      formats: ['es', 'cjs'],
-      fileName: format => `turtle-widget.${format}.js`,
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
+        // assetFileNames: 'assets/[name][extname]',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
     },
+    // sourcemap: true,
+    emptyOutDir: true,
   },
 })
