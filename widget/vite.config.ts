9550b8849a6be99a5e17b40e11e10bdcd1bea2fb
Victor-Salomon <victor-salomon@hotmail.fr>
2025-03-04 12:22:15 +0100

    update configs

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,34 +1,19 @@
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 import tailwindcss from 'tailwindcss'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-// import copy from 'rollup-plugin-copy'
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
       tsconfigPath: 'tsconfig.app.json',
-      outDir: 'dist', // put .d.ts files in ./dist
-      insertTypesEntry: true, // create an index.d.ts entry
-      // rollupTypes: true,
+      outDir: 'dist',
+      insertTypesEntry: true,
     }),
     cssInjectedByJsPlugin(),
-    // {
-    //   ...copy({
-    //     targets: [
-    //       // Copy entire subfolders from src/assets
-    //       { src: 'src/assets/fonts', dest: 'dist/src/assets' },
-    //       { src: 'src/assets/logos', dest: 'dist/src/assets' },
-    //       { src: 'src/assets/svg', dest: 'dist/src/assets' },
-    //       { src: 'src/assets/wallets-logo', dest: 'dist/src/assets' },
-    //     ],
-    //     hook: 'writeBundle',
-    //   }),
-    //   apply: 'build',
-    // },
   ],
   css: {
     postcss: {
@@ -48,7 +33,6 @@ export default defineConfig({
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
-        // assetFileNames: 'assets/[name][extname]',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
@@ -56,7 +40,6 @@ export default defineConfig({
         },
       },
     },
-    // sourcemap: true,
     emptyOutDir: true,
   },
 })
