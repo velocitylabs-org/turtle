01a9b5cab34f26f69ef16587a62549d10de261cc
Victor-Salomon <victor-salomon@hotmail.fr>
2025-03-11 14:38:08 +0100

    add dev local page

---
+++ widget/src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Widget } from '@/components/Widget'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Widget />
+    </div>
+  </StrictMode>,
+)
