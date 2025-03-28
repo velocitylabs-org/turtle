616cd207096424a7af38f8629cdc263cd1eba127
Jonatan del Valle <delvallejonatan@gmail.com>
2025-03-28 15:14:31 -0300

    Refactor tab management and rename core components.
    Replaced "Menu" with "TabNavigation" and unified tab state handling with `TabOptions`. Renamed key components like "HomeComponentSelect" to "AppHome" for consistency. Removed redundant "DisplaysTransfers" interface and streamlined props.

---
+++ app/src/components/TabNavigation.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/utils/cn'
+import Button from './Button'
+import { TabOptions } from '@/models/transfer'
+import { Dispatch, SetStateAction } from 'react'
+
+interface TabNavigationProps {
+  selectedTab: TabOptions
+  setSelectedTab: Dispatch<SetStateAction<TabOptions>>
+  hasCompletedTransfers: boolean
+}
+
+export default function TabNavigation({
+  selectedTab,
+  setSelectedTab,
+  hasCompletedTransfers,
+}: TabNavigationProps) {
+  const isNewTabSelected = selectedTab === 'New'
+  return (
+    <div className="relative flex items-center gap-2">
+      <Button
+        variant={isNewTabSelected ? 'primary' : 'ghost'}
+        size="lg"
+        className="xl-letter-spacing relative z-10 rounded-2xl text-xl sm:text-large"
+        onClick={() => !isNewTabSelected && setSelectedTab('New')}
+      >
+        <span className={cn(isNewTabSelected ? 'text-black' : 'text-white')}>New</span>
+      </Button>
+      <Button
+        variant={!isNewTabSelected ? 'primary' : 'ghost'}
+        className="xl-letter-spacing relative z-10 rounded-2xl text-xl sm:text-large"
+        size="lg"
+        disabled={!hasCompletedTransfers}
+        onClick={() => isNewTabSelected && setSelectedTab('Done')}
+      >
+        <span className={cn('text-large', !isNewTabSelected ? 'text-black' : 'text-white')}>
+          Done
+        </span>
+      </Button>
+    </div>
+  )
+}
