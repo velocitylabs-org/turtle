18db57b32673925d762b4c849afeb2dc47e03070
steoo <stefano@velocitylabs.org>
2025-04-09 17:53:57 +0200

    prettier

--- app/src/hooks/useCompletedTransfers.ts
+++ app/src/hooks/useCompletedTransfers.ts
@@ -1,7 +1,6 @@
 import { useCompletedTransfersStore } from '@/store/completedTransfersStore'
 import useStore from './useStore'
 
-
 const useCompletedTransfers = () => {
   const completedTransfers = useStore(useCompletedTransfersStore, state => state.completedTransfers)
   const addCompletedTransfer = useCompletedTransfersStore(state => state.addCompletedTransfer)
