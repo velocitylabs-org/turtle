8060e646af68a5e528b2a44e7654e570ccfdd304
Noah Joeris <noahjoeris@gmail.com>
2025-04-09 14:26:27 +0300

    add migration function to ongoing storage

--- app/src/store/ongoingTransfersStore.ts
+++ app/src/store/ongoingTransfersStore.ts
@@ -1,4 +1,4 @@
-import { AmountInfo, StoredTransfer } from '@/models/transfer'
+import { AmountInfo, StoredTransfer, StoredTransferV0 } from '@/models/transfer'
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
@@ -95,6 +95,44 @@ export const useOngoingTransfersStore = create<State>()(
       name: 'turtle-ongoing-transactions',
       storage: createJSONStorage(() => localStorage),
       version: currentVersion,
+      migrate: (persistedState, version) => {
+        if (version === currentVersion) return persistedState
+
+        if (version === 0) {
+          const oldTransfers = persistedState as { transfers: StoredTransferV0[] }
+
+          const migratedTransfers = oldTransfers.transfers.map(transfer => ({
+            // Copy all fields from RawTransfer
+            id: transfer.id,
+            sourceChain: transfer.sourceChain,
+            destChain: transfer.destChain,
+            sender: transfer.sender,
+            recipient: transfer.recipient,
+            date: transfer.date,
+            crossChainMessageHash: transfer.crossChainMessageHash,
+            parachainMessageId: transfer.parachainMessageId,
+            sourceChainExtrinsicIndex: transfer.sourceChainExtrinsicIndex,
+            fees: transfer.fees,
+            bridgingFee: transfer.bridgingFee,
+            environment: transfer.environment,
+            sendResult: transfer.sendResult,
+            uniqueTrackingId: transfer.uniqueTrackingId,
+            status: transfer.status,
+            finalizedAt: transfer.finalizedAt,
+
+            // V0 Migrations
+            sourceToken: transfer.token,
+            sourceAmount: transfer.amount,
+            sourceTokenUSDValue: transfer.tokenUSDValue,
+            swapInformation: undefined,
+          }))
+
+          return { transfers: migratedTransfers }
+        }
+
+        console.warn(`Unknown ongoing transfers version ${version}, resetting to initial state`)
+        return { transfers: [] }
+      },
     },
   ),
 )
