711f57b88b5214513821311a1d2ddc07fed17482
Noah Joeris <noahjoeris@gmail.com>
2025-04-15 15:42:16 +0300

    cleanup: remove logs

--- app/src/hooks/useOutputAmount.tsx
+++ app/src/hooks/useOutputAmount.tsx
@@ -46,13 +46,6 @@ export function useOutputAmount({
         return null
 
       try {
-        console.log(
-          fees,
-          sourceToken.id,
-          destinationToken.id,
-          fees?.token.id === sourceToken.id,
-          fees?.amount,
-        )
         // Normal transfer
         if (isSameToken(sourceToken, destinationToken)) {
           if (fees && fees.token.id === sourceToken.id) return BigInt(amount) - BigInt(fees.amount)
