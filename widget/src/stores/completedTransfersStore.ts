fd89d1f17ea21ad4656aa9483ff78d6ec048018c
Victor-Salomon <victor-salomon@hotmail.fr>
2025-04-16 17:35:42 +0200

    allow completed transfer store versioning

--- widget/src/stores/completedTransfersStore.ts
+++ widget/src/stores/completedTransfersStore.ts
@@ -1,4 +1,4 @@
-import { AmountInfo, CompletedTransfer } from '@/models/transfer'
+import { AmountInfo, CompletedTransfer, CompletedTransferV0 } from '@/models/transfer'
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
@@ -14,7 +14,7 @@ const serializeFeeAmount = (fees: AmountInfo): AmountInfo => {
   }
 }
 
-// const currentStoreVersion = 1
+const currentStoreVersion = 1
 
 export const useCompletedTransfersStore = create<CompletedTxState>()(
   persist(
@@ -51,40 +51,40 @@ export const useCompletedTransfersStore = create<CompletedTxState>()(
       name: 'turtle-completed-transactions',
       storage: createJSONStorage(() => localStorage),
       partialize: state => ({ completedTransfers: state.completedTransfers }),
-      // version: currentStoreVersion,
-      // migrate: (persistedState, version) => {
-      //   if (version === currentStoreVersion) return persistedState
+      version: currentStoreVersion,
+      migrate: (persistedState, version) => {
+        if (version === currentStoreVersion) return persistedState
 
-      //   if (version === 0) {
-      //     const oldTransfers = persistedState as { completedTransfers: CompletedTransferV0[] }
+        if (version === 0) {
+          const oldTransfers = persistedState as { completedTransfers: CompletedTransferV0[] }
 
-      //     const migratedTransfers = oldTransfers.completedTransfers.map(transfer => ({
-      //       id: transfer.id,
-      //       result: transfer.result,
-      //       sourceChain: transfer.sourceChain,
-      //       destChain: transfer.destChain,
-      //       sender: transfer.sender,
-      //       recipient: transfer.recipient,
-      //       date: transfer.date,
-      //       fees: transfer.fees,
-      //       bridgingFee: transfer.bridgingFee,
-      //       minTokenRecieved: transfer.minTokenRecieved,
-      //       minTokenRecievedValue: transfer.minTokenRecievedValue,
-      //       explorerLink: transfer.explorerLink,
-      //       errors: transfer.errors,
+          const migratedTransfers = oldTransfers.completedTransfers.map(transfer => ({
+            id: transfer.id,
+            result: transfer.result,
+            sourceChain: transfer.sourceChain,
+            destChain: transfer.destChain,
+            sender: transfer.sender,
+            recipient: transfer.recipient,
+            date: transfer.date,
+            fees: transfer.fees,
+            bridgingFee: transfer.bridgingFee,
+            minTokenRecieved: transfer.minTokenRecieved,
+            minTokenRecievedValue: transfer.minTokenRecievedValue,
+            explorerLink: transfer.explorerLink,
+            errors: transfer.errors,
 
-      //       // V0 Migrations
-      //       sourceToken: transfer.token,
-      //       sourceTokenUSDValue: transfer.tokenUSDValue,
-      //       sourceAmount: transfer.amount,
-      //     }))
+            // V0 Migrations
+            sourceToken: transfer.token,
+            sourceTokenUSDValue: transfer.tokenUSDValue,
+            sourceAmount: transfer.amount,
+          }))
 
-      //     return { completedTransfers: migratedTransfers }
-      //   }
+          return { completedTransfers: migratedTransfers }
+        }
 
-      //   console.warn(`Unknown completed transfers version ${version}, resetting to initial state`)
-      //   return { completedTransfers: [] }
-      // },
+        console.warn(`Unknown completed transfers version ${version}, resetting to initial state`)
+        return { completedTransfers: [] }
+      },
     },
   ),
 )
