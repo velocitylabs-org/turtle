46d9371d275aad0ce967085c3e5008e3ae71c608
steoo <stefano@velocitylabs.org>
2025-04-18 14:09:32 +0200

    fixed heroui for the widget

--- widget/tailwind.config.ts
+++ widget/tailwind.config.ts
@@ -49,7 +49,7 @@ const config: import('tailwindcss').Config = {
   content: [
     './index.html',
     './src/**/*.{ts,tsx,js,jsx}',
-    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    '../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
