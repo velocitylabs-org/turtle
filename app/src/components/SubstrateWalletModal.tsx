90d831678823738ac714af2eea56dd61705dc70d
Jonatan del Valle <delvallejonatan@gmail.com>
2025-04-18 14:50:13 +0200

    Refactor SubstrateWalletModal animations and layout

--- app/src/components/SubstrateWalletModal.tsx
+++ app/src/components/SubstrateWalletModal.tsx
@@ -3,7 +3,7 @@ import useSubstrateWallet from '@/hooks/useSubstrateWallet'
 import { truncateAddress } from '@/utils/address'
 import { getWalletLogo, getWalletName, getWalletWeight } from '@/utils/wallet'
 import type { InjectedAccount, InjectedExtension } from '@polkadot/extension-inject/types'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { ChevronLeft } from 'lucide-react'
 import { useEffect, useMemo, useState } from 'react'
 import { colors } from '../../tailwind.config'
@@ -12,18 +12,23 @@ import Icon from './Icon'
 import LoadingIcon from './svg/LoadingIcon'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog'
 
-const footerAnimationProps = {
-  initial: { opacity: 0, height: 0 },
-  animate: { opacity: 1, height: 'auto' },
-  exit: { opacity: 0, height: 0 },
-  transition: { duration: 0.3 },
+const extensionsViewTransitions = {
+  initial: { x: -10, opacity: 0 },
+  animate: { x: 0, opacity: 1, transition: { duration: 0.15, type: 'spring' } },
+  exit: { x: -10, opacity: 0, transition: { duration: 0.15, type: 'spring' } },
+}
+
+const accountsViewTransitions = {
+  initial: { x: 5, opacity: 0 },
+  animate: { x: 0, opacity: 1, transition: { duration: 0.15, type: 'spring' } },
+  exit: { x: 5, opacity: 0, transition: { duration: 0.15, type: 'spring' } },
 }
 
 const headerElementAnimationProps = {
-  initial: { opacity: 0, y: 5 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -5 },
-  transition: { duration: 0.3 },
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.5, type: 'spring' },
 }
 
 export default function SubstrateWalletModal() {
@@ -78,9 +83,9 @@ export default function SubstrateWalletModal() {
 
   const heightAnimationProps = useMemo(
     () => ({
-      initial: { height: currentView === 'extensions' ? '10rem' : '11.8rem' },
-      animate: { height: currentView === 'extensions' ? '10rem' : '11.8rem' },
-      transition: { duration: 0.5, type: 'spring' },
+      initial: { height: currentView === 'extensions' ? '12.5rem' : '14rem' },
+      animate: { height: currentView === 'extensions' ? '12.5rem' : '14rem' },
+      transition: { duration: 0.3, type: 'spring' },
     }),
     [currentView],
   )
@@ -88,7 +93,7 @@ export default function SubstrateWalletModal() {
   return (
     <Dialog open={isModalOpen} onOpenChange={open => (open ? openModal() : closeModal())}>
       <DialogContent
-        className="m-auto max-h-[85vh] max-w-[90vw] rounded-4xl pb-4 focus:outline-none min-[460px]:max-w-[24rem]"
+        className="m-auto max-h-[85vh] max-w-[90vw] rounded-4xl focus:outline-none min-[460px]:max-w-[24rem]"
         hideCloseButton={false}
         overlayProps={{
           className: 'bg-black/70',
@@ -103,113 +108,142 @@ export default function SubstrateWalletModal() {
         {/* Header */}
         <DialogHeader className="flex items-center justify-center rounded-t-4xl p-4">
           {currentView === 'accounts' && (
-            <div className="absolute left-0">
+            <motion.div
+              key={currentView}
+              {...headerElementAnimationProps}
+              className="absolute left-0"
+            >
               <Button variant="ghost" size="md" onClick={() => setCurrentView('extensions')}>
-                <motion.span
-                  key={currentView}
-                  {...headerElementAnimationProps}
-                  className="flex h-[29px] w-[29px] items-center justify-center rounded-[10px] p-[3px] opacity-100 hover:bg-gray-100"
-                >
+                <span className="flex h-[29px] w-[29px] items-center justify-center rounded-[10px] p-[3px] opacity-100 hover:bg-gray-100">
                   <ChevronLeft className="h-5 w-5" strokeWidth={3} />
-                </motion.span>
+                </span>
               </Button>
-            </div>
+            </motion.div>
           )}
           <DialogTitle className="mt-1 text-base font-bold">
-            <motion.span key={currentView} {...headerElementAnimationProps}>
-              {currentView === 'extensions' ? 'Connect Wallet' : 'Connect Account'}
-            </motion.span>
+            <AnimatePresence mode="wait">
+              {currentView === 'extensions' && (
+                <motion.div key="extensionsView" {...extensionsViewTransitions}>
+                  Connect Wallet
+                </motion.div>
+              )}
+              {currentView === 'accounts' && (
+                <motion.div key="accountsView" {...accountsViewTransitions}>
+                  Connect Wallet
+                </motion.div>
+              )}
+            </AnimatePresence>
           </DialogTitle>
         </DialogHeader>
 
         {/* Animate Height Transition */}
         <motion.div
-          className="flex max-h-[15rem] min-h-[130px] flex-col items-center justify-start space-y-2 overflow-y-auto p-6 pt-3 text-base"
+          className="flex flex-col items-center justify-start space-y-2 overflow-y-auto p-6 pt-2 text-base"
           layout
           {...heightAnimationProps}
         >
-          {/* Loading */}
-          {loading && (
-            <div className="flex h-full w-full items-center justify-center">
-              <LoadingIcon
-                className="animate-spin"
-                width={spinnerSize['lg']}
-                height={spinnerSize['lg']}
-                color={colors['turtle-secondary']}
-              />
-            </div>
-          )}
-
-          {/* Show extensions */}
-          {currentView === 'extensions' &&
-            !loading &&
-            (extensions.length > 0 ? (
-              [...extensions]
-                .sort((a, b) => getWalletWeight(a.name, window) - getWalletWeight(b.name, window))
-                .map(extension => (
-                  <Button
-                    key={extension.name}
-                    className="flex min-h-12 w-full items-center justify-between rounded-[12px] border-0 bg-[#fafafa] p-4 data-[hover='true']:bg-[#f6f6f6] data-[hover='true']:opacity-100"
-                    variant="outline"
-                    onClick={() => handleExtensionSelect(extension)}
-                  >
-                    <div className="flex items-center space-x-2 text-sm">
-                      <Icon src={getWalletLogo(extension.name, window)} width={40} height={40} />
-                      <span className="block max-w-[120px] truncate sm:max-w-[165px]">
-                        {getWalletName(extension.name, window)}
-                      </span>
-                    </div>
-                    <span className="rounded-[5px] bg-turtle-primary-light px-[6px] py-[3px] text-[11px] font-bold text-turtle-primary-dark text-opacity-80">
-                      INSTALLED
-                    </span>
-                  </Button>
-                ))
-            ) : (
-              <div className="flex h-full w-full items-center justify-center">
-                <p className="text-center text-sm text-turtle-level6">
-                  <span className="font-bold">Oops! </span>No extensions detected. Please install a
-                  compatible wallet extension, such as Talisman, Subwallet, or Polkadot.js.
-                </p>
+          <AnimatePresence mode="wait">
+            {/* Loading */}
+            {loading && (
+              <div className="mb-3 flex h-full w-full items-center justify-center">
+                <LoadingIcon
+                  className="animate-spin"
+                  width={spinnerSize['lg']}
+                  height={spinnerSize['lg']}
+                  color={colors['turtle-secondary']}
+                />
               </div>
-            ))}
+            )}
 
-          {/* Show accounts */}
-          {currentView === 'accounts' &&
-            !loading &&
-            (filteredAccounts.length > 0 ? (
-              filteredAccounts.map(account => (
-                <Button
-                  key={account.address}
-                  className="flex min-h-12 w-full items-center justify-between rounded-[12px] border-0 bg-[#fafafa] p-4 data-[hover='true']:bg-[#f6f6f6] data-[hover='true']:opacity-100"
-                  variant="outline"
-                  onClick={() => handleAccountSelect(account)}
-                >
-                  <div className="flex flex-col items-start space-y-1 text-sm">
-                    <span className="block max-w-[180px] truncate text-[13px] font-medium sm:max-w-[220px]">
-                      {account.name || 'Unnamed Account'}
-                    </span>
-                    <span className="text-xs text-turtle-level6">
-                      {truncateAddress(account.address)}
-                    </span>
+            {currentView === 'extensions' && !loading && (
+              <motion.div
+                key="extensionsView"
+                {...extensionsViewTransitions}
+                className="flex w-full flex-1 flex-col"
+              >
+                {extensions.length > 0 ? (
+                  <div className="flex flex-1 flex-col gap-2">
+                    {/* Added wrapper div */}
+                    {[...extensions]
+                      .sort(
+                        (a, b) => getWalletWeight(a.name, window) - getWalletWeight(b.name, window),
+                      )
+                      .map(extension => (
+                        <Button
+                          key={extension.name}
+                          className='flex min-h-12 w-full items-center justify-between rounded-[12px] border-0 bg-[#fafafa] p-4 data-[hover="true"]:bg-[#f6f6f6] data-[hover="true"]:opacity-100'
+                          variant="outline"
+                          onClick={() => handleExtensionSelect(extension)}
+                        >
+                          <div className="flex items-center space-x-2 text-sm">
+                            <Icon
+                              src={getWalletLogo(extension.name, window)}
+                              width={40}
+                              height={40}
+                            />
+                            <span className="block max-w-[120px] truncate sm:max-w-[165px]">
+                              {getWalletName(extension.name, window)}
+                            </span>
+                          </div>
+                          <span className="rounded-[5px] bg-turtle-primary-light px-[6px] py-[3px] text-[11px] font-bold text-turtle-primary-dark text-opacity-80">
+                            INSTALLED
+                          </span>
+                        </Button>
+                      ))}
                   </div>
-                  <span className="rounded-[5px] bg-turtle-primary-light px-[6px] py-[3px] text-[11px] font-bold text-turtle-primary-dark text-opacity-80">
-                    CONNECT
-                  </span>
-                </Button>
-              ))
-            ) : (
-              <div className="flex h-full w-full items-center justify-center">
-                <p className="text-center text-sm text-turtle-level6">
-                  <span className="font-bold">Oops! </span>No accounts available. Please connect an
-                  account to Turtle inside your wallet extension.
-                </p>
-              </div>
-            ))}
-        </motion.div>
-
-        {/* Footer */}
-        <motion.div layout {...footerAnimationProps}>
-          {currentView === 'extensions' && <Footer />}
+                ) : (
+                  <div className="flex h-full w-full items-center justify-center">
+                    <p className="text-center text-sm text-turtle-level6">
+                      <span className="font-bold">Oops! </span>No extensions detected. Please
+                      install a compatible wallet extension, such as Talisman, Subwallet, or
+                      Polkadot.js.
+                    </p>
+                  </div>
+                )}
+                <Footer />
+              </motion.div>
+            )}
+            {/* Show accounts */}
+            {currentView === 'accounts' && !loading && (
+              <motion.div
+                key="accountsView"
+                {...accountsViewTransitions}
+                className="flex w-full flex-1 flex-col"
+              >
+                {filteredAccounts.length > 0 ? (
+                  <div className="flex flex-1 flex-col gap-2">
+                    {filteredAccounts.map(account => (
+                      <Button
+                        key={account.address}
+                        className='flex min-h-12 w-full items-center justify-between rounded-[12px] border-0 bg-[#fafafa] p-4 data-[hover="true"]:bg-[#f6f6f6] data-[hover="true"]:opacity-100'
+                        variant="outline"
+                        onClick={() => handleAccountSelect(account)}
+                      >
+                        <div className="flex flex-col items-start space-y-1 text-sm">
+                          <span className="block max-w-[180px] truncate text-[13px] font-medium sm:max-w-[220px]">
+                            {account.name || 'Unnamed Account'}
+                          </span>
+                          <span className="text-xs text-turtle-level6">
+                            {truncateAddress(account.address)}
+                          </span>
+                        </div>
+                        <span className="rounded-[5px] bg-turtle-primary-light px-[6px] py-[3px] text-[11px] font-bold text-turtle-primary-dark text-opacity-80">
+                          CONNECT
+                        </span>
+                      </Button>
+                    ))}
+                  </div>
+                ) : (
+                  <div className="flex h-full w-full items-center justify-center">
+                    <p className="text-center text-sm text-turtle-level6">
+                      <span className="font-bold">Oops! </span>No accounts available. Please connect
+                      an account to Turtle inside your wallet extension.
+                    </p>
+                  </div>
+                )}
+              </motion.div>
+            )}
+          </AnimatePresence>
         </motion.div>
       </DialogContent>
     </Dialog>
@@ -218,7 +252,7 @@ export default function SubstrateWalletModal() {
 
 function Footer() {
   return (
-    <div className="text mb-1 mt-4 text-center text-[12px] text-gray-500">
+    <div className="text mt-7 text-center text-[12px] leading-[13px] text-gray-500">
       Haven&apos;t got a wallet?
       <a
         href="https://support.polkadot.network/support/solutions/articles/65000098878-how-to-create-a-polkadot-account"
