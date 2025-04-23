fbb2489c76420f0e419643a723d7635969a8acfe
Noah Joeris <noahjoeris@gmail.com>
2025-04-23 15:40:02 +0300

    refactor: extract search bar into own component

--- app/src/components/ChainTokenSelect.tsx
+++ app/src/components/ChainTokenSelect.tsx
@@ -14,9 +14,9 @@ import { colors } from '../../tailwind.config'
 import Button from './Button'
 import ChainTrigger from './ChainTrigger'
 import Dropdown from './Dropdown'
+import SearchBar from './SearchBar'
 import ChevronDown from './svg/ChevronDown'
 import Cross from './svg/Cross'
-import SearchIcon from './svg/SearchIcon'
 import TokenIcon from './svg/TokenIcon'
 import TokenLogo from './TokenLogo'
 import Tooltip from './Tooltip'
@@ -453,31 +453,5 @@ const ClearButton = ({ onClick }: { onClick: () => void }) => {
         <Cross stroke={colors['turtle-secondary']} />
       </div>
     </Button>
-  )
-}
-
-const SearchBar = ({
-  placeholder,
-  value,
-  onChange,
-}: {
-  placeholder: string
-  value: string
-  onChange: (value: string) => void
-}) => {
-  return (
-    <div className="sticky top-0 z-20 flex items-center gap-2 border-b-1 border-turtle-level3 bg-turtle-background px-3 py-3">
-      <div className="flex h-[2rem] w-[2rem] shrink-0 items-center justify-center">
-        <SearchIcon fill={colors['turtle-level5']} width={17} height={17} />
-      </div>
-
-      <input
-        type="text"
-        value={value}
-        onChange={e => onChange(e.target.value)}
-        placeholder={placeholder}
-        className="w-full rounded-md border-0 bg-transparent text-sm placeholder:text-turtle-level5 focus:border-0 focus:outline-none"
-      />
-    </div>
   )
 }
