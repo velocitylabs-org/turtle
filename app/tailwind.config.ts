833026b1c39d4949e23961a8254d645045bf49e6
steoo <stefano@velocitylabs.org>
2025-04-30 15:46:08 +0200

    svg

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    'node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
