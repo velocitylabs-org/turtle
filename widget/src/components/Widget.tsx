3844af511a3bf498bbeb3855ce03b7fd15592979
Stefano <esseiproject@gmail.com>
2025-05-08 14:29:11 +0200

    Scope CSS for Widget (#370)
    Co-authored-by: steoo <stefano@velocitylabs.org>

--- widget/src/components/Widget.tsx
+++ widget/src/components/Widget.tsx
@@ -24,55 +24,57 @@ const Widget = ({ theme }: { theme?: WidgetTheme }) => {
   const TransfersHistory = lazy(() => import('@/components/history/TransfersHistory'))
 
   return (
-    <Providers>
-      <div className="m-4 flex flex-col items-center justify-center p-6">
-        <div className="relative">
-          {(ongoingTransfers.length > 0 ||
-            (completedTransfers && completedTransfers.length > 0)) && (
-            <div
-              className="absolute -top-5 right-8 z-30 max-w-[90vw] rounded-lg bg-turtle-background"
-              onClick={() => setNewTransferInit(newTransferInit === 'New' ? 'History' : 'New')}
-            >
-              <div className="animation-bounce relative m-1 cursor-pointer rounded-lg border p-3">
-                {ongoingTransfers.length > 0 && !isHistoryTabSelected && (
-                  <div className="text-foreground absolute -left-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full border border-turtle-secondary bg-turtle-background">
-                    <span className="text-xs">{ongoingTransfers.length}</span>
-                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-turtle-secondary-dark opacity-25" />
-                  </div>
-                )}
-                <div>
-                  {isHistoryTabSelected ? (
-                    <ArrowLeft className="h-4 w-4" />
-                  ) : (
-                    <History className="h-4 w-4" />
+    <div className="turtle-wrapper">
+      <Providers>
+        <div className="m-4 flex flex-col items-center justify-center p-6">
+          <div className="relative">
+            {(ongoingTransfers.length > 0 ||
+              (completedTransfers && completedTransfers.length > 0)) && (
+              <div
+                className="absolute -top-5 right-8 z-30 max-w-[90vw] rounded-lg bg-turtle-background"
+                onClick={() => setNewTransferInit(newTransferInit === 'New' ? 'History' : 'New')}
+              >
+                <div className="animation-bounce relative m-1 cursor-pointer rounded-lg border p-3">
+                  {ongoingTransfers.length > 0 && !isHistoryTabSelected && (
+                    <div className="text-foreground absolute -left-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full border border-turtle-secondary bg-turtle-background">
+                      <span className="text-xs">{ongoingTransfers.length}</span>
+                      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-turtle-secondary-dark opacity-25" />
+                    </div>
                   )}
+                  <div>
+                    {isHistoryTabSelected ? (
+                      <ArrowLeft className="h-4 w-4" />
+                    ) : (
+                      <History className="h-4 w-4" />
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-          {!isHistoryTabSelected ? (
-            <TransferForm />
-          ) : (
-            <Suspense
-              fallback={
-                <HistoryLoaderSkeleton
-                  length={
-                    ongoingTransfers.length + (completedTransfers ? completedTransfers.length : 0)
-                  }
+            )}
+            {!isHistoryTabSelected ? (
+              <TransferForm />
+            ) : (
+              <Suspense
+                fallback={
+                  <HistoryLoaderSkeleton
+                    length={
+                      ongoingTransfers.length + (completedTransfers ? completedTransfers.length : 0)
+                    }
+                  />
+                }
+              >
+                <TransfersHistory
+                  ongoingTransfers={ongoingTransfers}
+                  completedTransfers={completedTransfers ?? []}
                 />
-              }
-            >
-              <TransfersHistory
-                ongoingTransfers={ongoingTransfers}
-                completedTransfers={completedTransfers ?? []}
-              />
-            </Suspense>
-          )}
+              </Suspense>
+            )}
+          </div>
+          <NotificationSystem />
+          <SubstrateWalletModal />
         </div>
-        <NotificationSystem />
-        <SubstrateWalletModal />
-      </div>
-    </Providers>
+      </Providers>
+    </div>
   )
 }
 export default Widget
