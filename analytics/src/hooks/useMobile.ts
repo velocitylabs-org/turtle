9ce1242ddfaf6c7ed97341bd60c9dafa8602eb5b
Jonatan del Valle <delvallejonatan@gmail.com>
2025-05-16 13:24:09 +0200

    Handle potential undefined `window` in useIsMobile hook

--- analytics/src/hooks/useMobile.ts
+++ analytics/src/hooks/useMobile.ts
@@ -3,15 +3,15 @@ import { useEffect, useState } from 'react'
 const MOBILE_BREAKPOINT = 768
 
 export default function useIsMobile() {
-  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < MOBILE_BREAKPOINT)
+  const [isMobile, setIsMobile] = useState<boolean>(window?.innerWidth < MOBILE_BREAKPOINT)
 
   useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const mql = window?.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      setIsMobile(window?.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener('change', onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    setIsMobile(window?.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener('change', onChange)
   }, [])
 
