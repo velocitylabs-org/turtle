d1258ac6e5d4b9b9e198567b8ced2060fb852a3b
Jonatan del Valle <delvallejonatan@gmail.com>
2025-05-20 12:00:13 +0200

    Remove unused onNextClick prop from calendar component

--- analytics/src/components/ui/calendar.tsx
+++ analytics/src/components/ui/calendar.tsx
@@ -72,7 +72,7 @@ function Calendar({
     }, [yearRange]),
   )
 
-  const { onNextClick, onPrevClick, startMonth, endMonth } = props
+  const { onPrevClick, startMonth, endMonth } = props
   const columnsDisplayed = navView === 'years' ? 1 : numberOfMonths
 
   const _monthsClassName = cn('relative flex', props.monthsClassName)
