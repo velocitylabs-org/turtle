7b6bb76e182d6c2aea54634609dda4032670b218
Jonatan del Valle <delvallejonatan@gmail.com>
2025-05-20 11:18:06 +0200

    Integrate Sentry for error monitoring and handling.

---
+++ analytics/src/instrumentation.ts
@@ -0,0 +1,19 @@
+import { captureRequestError, init } from '@sentry/nextjs'
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      debug: false,
+    });
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      debug: false,
+    });
+  }
+}
+
+export const onRequestError = captureRequestError;
