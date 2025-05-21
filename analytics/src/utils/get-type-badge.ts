14af6f1d53eec4398d70640b2d45eee2bd0332e6
Jonatan del Valle <delvallejonatan@gmail.com>
2025-05-21 22:58:51 +0200

    Add pnpm-lock.yaml file

--- analytics/src/utils/get-type-badge.ts
+++ analytics/src/utils/get-type-badge.ts
@@ -5,9 +5,7 @@ import { getSrcFromLogo } from './get-src-from-logo'
 // This helper function returns the type badge for a given token (e.g., Ethereum or Polkadot)
 export default function getTypeBadge(tokenId: string) {
   const token = tokensById[tokenId]
-  let type
-  if (token.origin.type === 'Ethereum') type = ethereumChain
-  if (token.origin.type === 'Polkadot') type = relayChain
+  const type = token.origin.type === 'Ethereum' ? ethereumChain : relayChain
 
   return {
     logoURI: getSrcFromLogo(token),
