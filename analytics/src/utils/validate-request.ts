0872259358eca87d54feaae40e58fab38c340588
Jonatan del Valle <delvallejonatan@gmail.com>
2025-05-22 20:04:39 +0200

    Simplify request validation logic.

--- analytics/src/utils/validate-request.ts
+++ analytics/src/utils/validate-request.ts
@@ -5,23 +5,5 @@ export default function validateRequest(req: Request): boolean {
   const auth = req.headers.get('authorization')
   const expectedAuth = process.env.NEXT_PUBLIC_AUTH_TOKEN
 
-  if (!auth || !expectedAuth) {
-    return false
-  }
-
-  const origin = req.headers.get('origin')
-  if (!origin) {
-    return false
-  }
-
-  const allowedOrigins =
-    process.env.TRANSACTION_ALLOWED_ORIGINS?.split(',').map(o => o.trim()) || []
-  if (!allowedOrigins.length) {
-    return false
-  }
-
-  const isAllowedOrigin = allowedOrigins.includes(origin)
-  const isValidAuth = auth === expectedAuth
-
-  return isValidAuth && isAllowedOrigin
+  return auth === expectedAuth
 }
