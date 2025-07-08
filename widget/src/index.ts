fc764bbfb7ef9022c0cb922351a0c6d7cf5b0488
Victor Salomon <68683271+Victor-Salomon@users.noreply.github.com>
2025-07-08 16:08:16 +0200

    fix: widget pre-release improvements (#433)

--- widget/src/index.ts
+++ widget/src/index.ts
@@ -3,3 +3,4 @@ import Widget from '@/components/Widget'
 
 export default Widget
 export type { WidgetTheme, HexColor, RGBValues } from './utils/theme'
+export type { ConfigRegistryType } from './providers/ConfigProviders'
