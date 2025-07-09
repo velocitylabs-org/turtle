00455f881117d051aebe4472e899ec22d598f9a2
steoo <stefano@velocitylabs.org>
2025-07-09 18:41:15 +0200

    cleanups

--- widget/src/main.tsx
+++ widget/src/main.tsx
@@ -7,12 +7,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="flex h-screen w-screen items-center justify-center">
-      <Widget
-        registry={{
-          chains: ['hydration', 'moonbeam', 'polkadot', 'polkadot-assethub', 'bifrost'],
-          tokens: ['eth'],
-        }}
-      />
+      <Widget />
     </div>
   </StrictMode>,
 )
