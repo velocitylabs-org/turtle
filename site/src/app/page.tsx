d79ba003c9437892f7dc934ab67001a41601ef7e
Jonatan del Valle <delvallejonatan@gmail.com>
2025-07-18 16:33:02 +0200

    Display analytics data within the hero section and enhance styling of `AnalyticData` component.

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,6 +1,5 @@
 import Features from '@/components/features'
 import TurtlesBackground from '@/components/TurtlesBackground'
-import AnalyticData from '@/components/AnalyticData'
 import getAnalyticsData from '@/utils/get-analytics-data'
 
 export default async function Home() {
@@ -8,24 +7,14 @@ export default async function Home() {
 
   return (
     <>
-      <section className="relative z-20 flex h-[75vh] flex-col items-center justify-center">
-        <TurtlesBackground header="Frictionless cross-chain transfers" />
+      <section className="relative z-20 flex h-[78vh] flex-col items-center justify-center">
+        <TurtlesBackground header="Frictionless cross-chain transfers" volume={analyticsData?.totalVolumeUsd} />
       </section>
 
       {/* Features section */}
-      <section className="features relative z-40 mt-[-20px] bg-info pt-20">
+      <section className="features relative z-40 mt-[-20px] bg-info pb-36 pt-20 md:pb-72 lg:pb-96">
         <Features />
       </section>
-
-      {/* Analytics section */}
-      {analyticsData && (
-        <section className="relative z-50 mt-[-20px] flex flex-row items-center justify-center gap-8 bg-info pb-36 pt-20 md:pb-72 lg:pb-96">
-          <AnalyticData
-            volume={analyticsData.totalVolumeUsd}
-            transactions={analyticsData.totalTransactions}
-          />
-        </section>
-      )}
     </>
   )
 }
--- site/src/components/AnalyticData.tsx
+++ site/src/components/AnalyticData.tsx
@@ -1,18 +1,26 @@
 export default function AnalyticData({
   volume,
-  transactions,
 }: {
   volume: number
-  transactions: number
 }) {
   return (
-    <div>
-      <h1 className="text-3xl font-bold tracking-tighter sm:text-xl">
-        {volume.toLocaleString()} USD
-      </h1>
-      <h1 className="text-3xl font-bold tracking-tighter sm:text-xl">
-        {transactions.toLocaleString()} transactions
-      </h1>
+    <div
+      className="flex h-auto w-full flex-col items-center justify-center z-50 relative my-[8vw] md:my-[6vw] lg:my-[4vw]">
+      <div
+        className="flex items-center gap-2 bg-turtle-primary px-4 py-2 md:px-5 md:py-3 rounded-full border border-black">
+        <BoltIcon />
+        <span className="text-black text-[16px] md:text-[18px] lg:text-[20px]">
+          <span className="font-bold mr-1">${volume.toLocaleString()}</span> Total funds moved in Turtle
+        </span>
+      </div>
     </div>
+  )
+}
+
+function BoltIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.88899 12.5155L16.8 1L13.6 10.625H18.7635C19.213 10.625 19.4342 11.172 19.111 11.4845L7.2 23L10.4 13.375H5.23652C4.78699 13.375 4.5658 12.828 4.88899 12.5155Z" stroke="#001B04" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
   )
 }
