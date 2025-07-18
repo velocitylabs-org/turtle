77e6d28f4ec0be9b491502282430a2aab0adc99c
Victor-Salomon <victor-salomon@hotmail.fr>
2025-07-18 12:45:41 +0200

    fix lint

--- packages/registry/src/mainnet/tokens.ts
+++ packages/registry/src/mainnet/tokens.ts
@@ -25,8 +25,8 @@ import wbtcLogo from "@velocitylabs-org/turtle-assets/logos/wbtc.svg";
 import wethLogo from "@velocitylabs-org/turtle-assets/logos/weth.svg";
 import wstethLogo from "@velocitylabs-org/turtle-assets/logos/wsteth.svg";
 
-import { parachain, snowbridgeWrapped } from "../helpers";
 import { Token } from "@/types";
+import { parachain, snowbridgeWrapped } from "../helpers";
 
 // Tokens
 export const EthereumTokens = {
