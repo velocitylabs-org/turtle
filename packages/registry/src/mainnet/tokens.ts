28c1c927c5582829db89622727172751aa29b961
Victor-Salomon <victor-salomon@hotmail.fr>
2025-07-18 12:37:30 +0200

    fix tests

--- packages/registry/src/mainnet/tokens.ts
+++ packages/registry/src/mainnet/tokens.ts
@@ -25,7 +25,7 @@ import wbtcLogo from "@velocitylabs-org/turtle-assets/logos/wbtc.svg";
 import wethLogo from "@velocitylabs-org/turtle-assets/logos/weth.svg";
 import wstethLogo from "@velocitylabs-org/turtle-assets/logos/wsteth.svg";
 
-import { parachain, snowbridgeWrapped } from "@/helpers";
+import { parachain, snowbridgeWrapped } from "../helpers";
 import { Token } from "@/types";
 
 // Tokens
