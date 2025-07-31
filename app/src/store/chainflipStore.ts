c8cd6987b674433d9cf4e0a9fb54ba86d0da101e
Victor-Salomon <victor-salomon@hotmail.fr>
2025-07-31 11:28:28 +0200

    create chainflip Client store

---
+++ app/src/store/chainflipStore.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+import { SwapSDK, SwapSDKOptions } from '@chainflip/sdk/swap'
+
+interface ChainflipSdk {
+  sdk: SwapSDK | null
+  initSdk: () => SwapSDK
+}
+
+export const useChainflipSdk = create<ChainflipSdk>()((set, get) => ({
+  sdk: null,
+  initSdk: () => {
+    const chainflipSdk = get().sdk
+    if (chainflipSdk) return chainflipSdk
+
+    const options = {
+      network: 'mainnet',
+      enabledFeatures: { dca: true },
+    } as SwapSDKOptions
+
+    const swapSDK = new SwapSDK(options)
+
+    set({ sdk: swapSDK })
+    return swapSDK
+  },
+}))
