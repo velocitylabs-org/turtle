d23ac3e8be9f05c42f4d5d6283c3341f4d85fe8f
steoo <stefano@velocitylabs.org>
2025-07-31 13:41:32 +0200

    updated to next 15 for bundling issues

--- site/src/components/widget/WidgetWrapper.tsx
+++ site/src/components/widget/WidgetWrapper.tsx
@@ -1,15 +1,6 @@
 'use client'
 
-import dynamic from 'next/dynamic'
-
-const Widget = dynamic(() => import('@velocitylabs-org/turtle-widget'), {
-  loading: () => (
-    <div className="widget-loader flex h-[300px] items-center justify-center rounded-2xl bg-white md:h-[460px] md:w-[504px]">
-      Loading Turtle Widget...
-    </div>
-  ),
-  ssr: false,
-})
+import Widget from '@velocitylabs-org/turtle-widget'
 
 const WidgetWrapper = () => {
   return <Widget />
