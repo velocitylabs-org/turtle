566a41aedb257a7ea5713dc2aa8f68f35833d2fc
Jonatan del Valle <delvallejonatan@gmail.com>
2025-08-01 15:31:21 +0200

    Refactor: Simplify UI of Total Volume Display on Landing Page (#475)

--- site/src/components/AnalyticData.tsx
+++ site/src/components/AnalyticData.tsx
@@ -38,30 +38,16 @@ function AnalyticDataClient({ initialVolume }: AnalyticDataProps) {
 
   return (
     <div className="relative z-50 my-[8vw] flex h-auto w-full flex-col items-center justify-center md:my-[6vw] lg:my-[4vw]">
-      <div className="flex items-center gap-2 rounded-full border border-black bg-turtle-primary px-4 py-2 md:px-5 md:py-3">
-        <BoltIcon />
-        <span className="text-[16px] text-black md:text-[18px] lg:text-[20px]">
-          <span className="mr-1 inline-flex items-center justify-center font-bold">
+      <div className="flex items-center gap-2 rounded-[40px] border border-black bg-turtle-primary px-6 py-3">
+        <div className="flex-col items-center justify-center text-[16px] text-black md:text-[18px] lg:text-[20px]">
+          <div className="text-center text-[28px] font-bold sm:text-[32px]">
             <NumberFlow value={value} format={format} />
-          </span>{' '}
-          <span className="hidden min-[550px]:inline">Total funds moved in Turtle</span>
-          <span className="inline min-[550px]:hidden">Total funds moved</span>
-        </span>
+          </div>
+          <div className="relative top-[-5px] text-[18px] leading-tight sm:text-[20px]">
+            Funds moved through Turtle
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
-
-function BoltIcon() {
-  return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M4.88899 12.5155L16.8 1L13.6 10.625H18.7635C19.213 10.625 19.4342 11.172 19.111 11.4845L7.2 23L10.4 13.375H5.23652C4.78699 13.375 4.5658 12.828 4.88899 12.5155Z"
-        stroke="#001B04"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
   )
 }
