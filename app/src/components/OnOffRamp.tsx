46083774d7d83a4fe39274ea6e93e3cc336fad03
Noah Joeris <noahjoeris@gmail.com>
2025-08-20 17:54:52 +0300

    finish integrating meld

--- app/src/components/OnOffRamp.tsx
+++ app/src/components/OnOffRamp.tsx
@@ -2,12 +2,13 @@ const OnOffRamp = () => {
   const meldApiKey = process.env.NEXT_PUBLIC_MELD_API_KEY
 
   return (
-    <div className="flex w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
+    <div className="flex pb-5 w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
       <iframe
         src={`https://sb.meldcrypto.com/?theme=lightMode&publicKey=${meldApiKey}`}
-        height="780"
+        height="700"
         width="450"
         title="Meld Widget"
+        className="bg-white"
       />
     </div>
   )
