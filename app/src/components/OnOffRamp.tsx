2141458481ba95e960927e0a27d30b83eae7580f
Noah Joeris <noahjoeris@gmail.com>
2025-08-26 14:54:05 +0300

    fix padding

--- app/src/components/OnOffRamp.tsx
+++ app/src/components/OnOffRamp.tsx
@@ -2,7 +2,7 @@ const OnOffRamp = () => {
   const meldApiKey = process.env.NEXT_PUBLIC_MELD_API_KEY
 
   return (
-    <div className="flex pb-5 w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
+    <div className="flex p-[40px] w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
       <iframe
         src={`https://meldcrypto.com/?theme=lightMode&publicKey=${meldApiKey}`}
         height="790"
