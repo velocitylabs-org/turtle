80c52941632c866ceb7795ca18742e60ac46f6e3
Noah Joeris <noahjoeris@gmail.com>
2025-08-27 19:21:43 +0300

    remove upper padding for meld

--- packages/ui/src/components/MeldWidget.tsx
+++ packages/ui/src/components/MeldWidget.tsx
@@ -1,6 +1,6 @@
 export const MeldWidget = ({ apiKey }: { apiKey: string }) => {
   return (
-    <div className="flex p-[40px] w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
+    <div className="flex px-[40px] pb-[40px] w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
       <iframe
         src={`https://meldcrypto.com/?theme=lightMode&publicKey=${apiKey}`}
         height="790"
