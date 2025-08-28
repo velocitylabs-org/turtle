3b364ab15de24bd44de38c68b4a7dbe1db9bb480
Noah Joeris <noahjoeris@gmail.com>
2025-08-28 15:07:41 +0300

    fix responsiveness

--- packages/ui/src/components/MeldWidget.tsx
+++ packages/ui/src/components/MeldWidget.tsx
@@ -3,10 +3,8 @@ export const MeldWidget = ({ apiKey }: { apiKey: string }) => {
     <div className="flex px-[40px] pb-[40px] w-full max-w-[90vw] flex-col gap-1 rounded-b-3xl border border-t-0 border-turtle-foreground bg-white sm:w-[31.5rem] relative items-center justify-center">
       <iframe
         src={`https://meldcrypto.com/?theme=lightMode&publicKey=${apiKey}`}
-        height="790"
-        width="450"
         title="Meld Widget"
-        className="border rounded border-white"
+        className="border rounded border-white sm:w-[450px] max-w-full sm:h-[790px] h-[670px]"
       />
     </div>
   )
