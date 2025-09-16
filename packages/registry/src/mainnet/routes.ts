e2c2f914d95e368eb7cbd4d3deecb93b02565a3b
Jonatan del Valle <delvallejonatan@gmail.com>
2025-09-17 00:19:49 +0200

    Refactor: Enable USDT and USDC support in mainnet routes after PAPI and Paraspell upgrade

--- packages/registry/src/mainnet/routes.ts
+++ packages/registry/src/mainnet/routes.ts
@@ -358,8 +358,8 @@ export const routes: Route[] = [
       PolkadotTokens.DOT.id,
       PolkadotTokens.GLMR.id,
       PolkadotTokens.VDOT.id,
-      // PolkadotTokens.USDT.id, // TODO: PAPI issue here, add this after upgrading to newer version of PAPI and Paraspell SDK
-      // PolkadotTokens.USDC.id, // TODO: PAPI issue here, add this after upgrading to newer version of PAPI and Paraspell SDK
+      PolkadotTokens.USDT.id,
+      PolkadotTokens.USDC.id,
       // PolkadotTokens.SOL.id
     ],
   },
