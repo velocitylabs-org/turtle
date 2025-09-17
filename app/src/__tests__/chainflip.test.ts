b319054768f6ac8bc9c77a2908be748930d05f60
Victor-Salomon <victor-salomon@hotmail.fr>
2025-09-17 15:32:18 +0200

    update chainflip test

--- app/src/__tests__/chainflip.test.ts
+++ app/src/__tests__/chainflip.test.ts
@@ -1,15 +1,15 @@
 import { describe, expect, it } from '@jest/globals'
-import { AssetHub, Ethereum, Polkadot } from '@velocitylabs-org/turtle-registry'
+import { Arbitrum, AssetHub, Ethereum, Polkadot } from '@velocitylabs-org/turtle-registry'
 import { type ChainflipChain, chainflipToRegistryChain } from '@/utils/chainflip'
 
-const ChainflipChains: ChainflipChain[] = ['Ethereum', 'Polkadot', 'Assethub']
+const ChainflipChains: ChainflipChain[] = ['Ethereum', 'Polkadot', 'Assethub', 'Arbitrum']
 
 describe('Chainflip Chains', () => {
   it('should match the corresponding Turtle registry chains', () => {
     const registryChains = ChainflipChains.map(chain => {
       return chainflipToRegistryChain(chain)
     })
-    expect(registryChains).toStrictEqual([Ethereum, Polkadot, AssetHub])
+    expect(registryChains).toStrictEqual([Ethereum, Polkadot, AssetHub, Arbitrum])
     expect(() => chainflipToRegistryChain('Unknown' as ChainflipChain)).toThrow()
   })
 })
