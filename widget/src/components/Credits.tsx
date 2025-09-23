c3337143f6e07c968f375bb5eef9cf11832c853f
Noah Joeris <noahjoeris@gmail.com>
2025-09-23 16:44:32 +0300

    migrate credits

---
+++ widget/src/components/Credits.tsx
@@ -0,0 +1,30 @@
+export default function Credits() {
+  return (
+    <div className="credits rainbow-text mt-6 flex flex-col text-center text-xs tracking-normal">
+      <div>
+        Made with love by{' '}
+        <a href="https://www.velocitylabs.org" target="_blank" rel="noopener noreferrer">
+          Velocity Labs
+        </a>
+      </div>
+      <div className="mt-1">
+        Powered by{' '}
+        <a href="https://docs.snowbridge.network" target="_blank" rel="noopener noreferrer">
+          Snowbridge
+        </a>
+        {', '}
+        <a href="https://paraspell.github.io/docs/" target="_blank" rel="noopener noreferrer">
+          ParaSpell
+        </a>
+        {', '}
+        <a href="https://chainflip.io/" target="_blank" rel="noopener noreferrer">
+          Chainflip
+        </a>
+        {' & '}
+        <a href="https://www.meld.io/" target="_blank" rel="noopener noreferrer">
+          Meld.io
+        </a>
+      </div>
+    </div>
+  )
+}
