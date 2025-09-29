4d4cb932f856dbbc3e23b8e0519bc2934dd8ccca
Noah Joeris <noahjoeris@gmail.com>
2025-09-29 20:05:44 +0300

    migrate some files

---
+++ widget/src/components/TransferEstimate.tsx
@@ -0,0 +1,16 @@
+import useTransferProgress from '@/hooks/useTransferProgress'
+import type { StoredTransfer } from '@/models/transfer'
+import type { Direction } from '@/utils/transfer'
+import ProgressBar from './ProgressBar'
+
+interface TransferEstimateProps {
+  transfer: StoredTransfer
+  direction: Direction
+  outlinedProgressBar: boolean
+}
+
+export default function TransferEstimate({ transfer, direction, outlinedProgressBar }: TransferEstimateProps) {
+  const progress = useTransferProgress(transfer, direction)
+
+  return <ProgressBar progress={transfer.progress ?? progress} outlinedProgressBar={outlinedProgressBar} />
+}
