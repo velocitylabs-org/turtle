2da879f952209db6a7e38c14832ddf5b953d09a2
Jonatan del Valle <delvallejonatan@gmail.com>
2025-10-02 12:04:10 +0200

    Feat: Add retry logic and improve cleanup process in `clean.sh` script (#545)

--- app/src/components/AppHome.tsx
+++ app/src/components/AppHome.tsx
@@ -1,16 +1,5 @@
 'use client'
-import { Body, LoadingIcon } from '@velocitylabs-org/turtle-ui'
-import dynamic from 'next/dynamic'
-
-const Widget = dynamic(() => import('@velocitylabs-org/turtle-widget'), {
-  ssr: false,
-  loading: () => (
-    <div className="widget-loader flex h-[300px] w-full flex-col items-center justify-center rounded-3xl bg-white md:h-[460px] md:w-[504px]">
-      <LoadingIcon className="animate-spin" width={40} height={40} />
-      <Body>Loading the widget...</Body>
-    </div>
-  ),
-})
+import Widget from '@velocitylabs-org/turtle-widget'
 
 export default function AppHome() {
   return <Widget endpointUrl="/" />
