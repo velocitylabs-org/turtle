c03ca017ae1de3e08b8707ebdb10266a50c71573
Victor Salomon <68683271+Victor-Salomon@users.noreply.github.com>
2025-11-10 13:55:25 +0100

    fix: svgs & app storage (#560)

--- app/src/components/svg/Cross.tsx
+++ app/src/components/svg/Cross.tsx
@@ -8,8 +8,8 @@ interface CrossProps extends ComponentPropsWithoutRef<'svg'> {
 export default function Cross({ stroke = colors['turtle-secondary'], ...props }: CrossProps) {
   return (
     <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M7.33331 0.666687L0.666647 7.33335" stroke={stroke} stroke-linecap="round" />
-      <path d="M0.666687 0.666687L7.33335 7.33335" stroke={stroke} stroke-linecap="round" />
+      <path d="M7.33331 0.666687L0.666647 7.33335" stroke={stroke} strokeLinecap="round" />
+      <path d="M0.666687 0.666687L7.33335 7.33335" stroke={stroke} strokeLinecap="round" />
     </svg>
   )
 }
--- app/src/components/svg/LoadingIcon.tsx
+++ app/src/components/svg/LoadingIcon.tsx
@@ -31,10 +31,10 @@ export default function LoadingIcon({
       <path
         d="M4.81081 1.61894C5.76664 1.03293 6.96543 0.829547 8.13794 1.15427C9.85203 1.62899 11.0011 3.09141 11.1173 4.72448V6.60038M9.20764 8.38106C8.2518 8.96707 7.05302 9.17045 5.88051 8.84573C4.0834 8.34802 2.90735 6.76461 2.8916 5.03713V3.78653"
         stroke={color}
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
-      <path d="M1 5.53863L2.87053 3.6742L4.89315 5.53847" stroke={color} stroke-linecap="round" />
-      <path d="M13 5.00001L11.1295 6.86444L9.10685 5.00017" stroke={color} stroke-linecap="round" />
+      <path d="M1 5.53863L2.87053 3.6742L4.89315 5.53847" stroke={color} strokeLinecap="round" />
+      <path d="M13 5.00001L11.1295 6.86444L9.10685 5.00017" stroke={color} strokeLinecap="round" />
     </svg>
   )
 }
