137bad3d1bacb28f6710eb0d991c22ddd7bb13d6
Nuno Alexandre <hi@nunoalexandre.com>
2026-01-14 10:52:32 +0100

    Add shutdown banner

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,29 +1,38 @@
 'use client'
-import { Tooltip } from '@velocitylabs-org/turtle-ui'
-import Link from 'next/link'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import ActionBanner from './ActionBanner'
 import TurtleIcon from './svg/TurtleIcon'
+
+const readMoreButton = {
+  label: 'Learn more',
+  onClick: () => {
+    window.open('https://medium.com/todo', '_blank', 'noopener,noreferrer')
+  },
+}
 
 export default function NavBar() {
   return (
-    <header className="relative z-50 flex w-full items-center justify-between px-6 py-5 sm:px-10 sm:py-9">
-      <Link href="/" className="flex items-center space-x-2">
-        <TurtleIcon size={40} />
-        <div className="turtle-text-shadow xl-letter-spacing text-[2rem] text-white">Turtle</div>
-        <div className="box-shadow mt-[1px] flex h-[19px] items-center justify-center rounded-[4px] border border-white bg-white px-[5px]">
-          <Tooltip showIcon={false} content="Turtle now supports swaps 🚀🫶🏻🥳">
-            <p className="rainbow-text text-[12px] font-bold">v3.0.0</p>
-          </Tooltip>
-        </div>
-      </Link>
-      <nav>
-        <ul className="items-center space-x-6">
-          <li>
-            <a href="https://turtle.cool" target="_blank" className="turtle-text-shadow text-white" rel="noopener">
-              About
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="relative z-50 flex flex-col w-full items-center mt-[7rem] justify-between px-6 py-5 sm:px-10 sm:py-9">
+        <>
+          <TurtleIcon size={40} />
+          <div className="turtle-text-shadow xl-letter-spacing text-[2rem] text-white">Turtle</div>
+          <div className="mt-[1px] flex h-[19px] items-center justify-center rounded-[4px] border border-white px-[5px]">
+            <p className="text-[12px] font-bold text-white">DEPRECATED</p>
+          </div>
+        </>
+
+        <motion.div className="flex items-center gap-1 self-center pt-1 w-[504px] text-center items-center">
+          <ActionBanner
+            disabled={false}
+            header="Turtle is being deprecated."
+            text={`Turtle will be shut down on February 15th 2026. Use at own risk meanwhile.`}
+            image={<Image src="/wip.png" alt="Announcement" width={86} height={86} />}
+            btn={readMoreButton}
+          />
+        </motion.div>
+      </header>
+    </>
   )
 }
